bf9c421f588fe13d2abf4902cd2cb42073c172a6
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-07-03 19:48:09 +0300

    Update arbitrage_trading.py

--- arbitrage_trading.py
+++ arbitrage_trading.py
@@ -1,24 +1,29 @@
+import requests
 import pandas as pd
 import streamlit as st
 
 def calculate_arbitrage(df, base_coin, investment_coin):
-    filtered_df = df[(df.symbol.str.endswith(base_coin)) & (df.symbol.str.startswith(investment_coin))]
-    filtered_df['profit_margin'] = ((filtered_df.bidPrice - filtered_df.askPrice) / filtered_df.askPrice) * 100
+    filtered_df = df[(df['FROMSYMBOL'] == base_coin) & (df['TOSYMBOL'] == investment_coin)]
+    filtered_df['profit_margin'] = ((filtered_df['BID'] - filtered_df['ASK']) / filtered_df['ASK']) * 100
     sorted_df = filtered_df.sort_values(by='profit_margin', ascending=False)
     return sorted_df
 
 def main():
-    st.title('Binance Arbitrage Trading')
-    df = pd.read_json('https://api.binance.com/api/v3/ticker/24hr')
-    coins = df.symbol.str.extract(r'(\w+)(\w+)')
-    coins = coins[1].unique()
+    st.title('CryptoCompare Arbitrage Trading')
+    url = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=*&tsyms=USD'
+    response = requests.get(url)
+    data = response.json()
+    raw_data = data['RAW']
+    df = pd.DataFrame.from_dict(raw_data).transpose()
+
+    coins = df.index.tolist()
     base_coin = st.selectbox('Select the base coin:', coins)
     investment_coin = st.selectbox('Select the investment coin:', coins)
 
     sorted_df = calculate_arbitrage(df, base_coin, investment_coin)
 
     st.write(f"Profit margins for {base_coin} to {investment_coin}:")
-    st.dataframe(sorted_df[['symbol', 'profit_margin']])
+    st.dataframe(sorted_df[['MARKET', 'profit_margin']])
 
 if __name__ == '__main__':
     main()
