703bf57af0dc98a64c206b3833cf428e54e1950a
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-07-03 19:49:49 +0300

    Update arbitrage_trading.py

--- arbitrage_trading.py
+++ arbitrage_trading.py
@@ -13,17 +13,21 @@ def main():
     url = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=*&tsyms=USD'
     response = requests.get(url)
     data = response.json()
-    raw_data = data['RAW']
-    df = pd.DataFrame.from_dict(raw_data).transpose()
-
-    coins = df.index.tolist()
-    base_coin = st.selectbox('Select the base coin:', coins)
-    investment_coin = st.selectbox('Select the investment coin:', coins)
-
-    sorted_df = calculate_arbitrage(df, base_coin, investment_coin)
-
-    st.write(f"Profit margins for {base_coin} to {investment_coin}:")
-    st.dataframe(sorted_df[['MARKET', 'profit_margin']])
+    
+    if 'RAW' in data:
+        raw_data = data['RAW']
+        df = pd.DataFrame.from_dict(raw_data).transpose()
+        
+        coins = df.index.tolist()
+        base_coin = st.selectbox('Select the base coin:', coins)
+        investment_coin = st.selectbox('Select the investment coin:', coins)
+        
+        sorted_df = calculate_arbitrage(df, base_coin, investment_coin)
+        
+        st.write(f"Profit margins for {base_coin} to {investment_coin}:")
+        st.dataframe(sorted_df[['MARKET', 'profit_margin']])
+    else:
+        st.write("Failed to retrieve data from CryptoCompare API")
 
 if __name__ == '__main__':
     main()
