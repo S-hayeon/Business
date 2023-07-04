42bb7af0d4c1399346186d285df9df04d259d6a8
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-07-04 14:01:41 +0300

    Update arbitrage_bin.py

--- arbitrage_bin.py
+++ arbitrage_bin.py
@@ -3,40 +3,39 @@ import requests
 import pandas as pd
 
 # Fetch data from Tiingo API
-def fetch_data(coin):
-    url = f"https://api.tiingo.com/tiingo/crypto/prices?tickers={coin}"
+def fetch_data():
+    import requests
     headers = {
-        'Content-Type': 'application/json',
-        'Authorization': 'Token ' + st.secrets['TIINGO_API']
-    }
-    response = requests.request("GET", url, headers=headers)
-    data = response.json()
+    'Content-Type': 'application/json' }
+    requestResponse = requests.get("https://api.tiingo.com/tiingo/crypto/prices?tickers=btcusd&startDate=2019-01-02&resampleFreq=5min&token=st.secrets['TIINGO_API']", headers=headers)
+    st.write(requestResponse.json())
     return data
 
 # Calculate arbitrage
 def calculate_arbitrage(base_coin, investment_coin):
     # Fetch data for base and investment coins
-    base_data = fetch_data(base_coin)
-    investment_data = fetch_data(investment_coin)
+    # base_data = fetch_data(base_coin)
+    # investment_data = fetch_data(investment_coin)
 
-    # Calculate arbitrage (this is a simplified example, you'll need to implement your own logic here)
-    arbitrage = investment_data[0]['price'] - base_data[0]['price']
+    # # Calculate arbitrage (this is a simplified example, you'll need to implement your own logic here)
+    # arbitrage = investment_data[0]['price'] - base_data[0]['price']
 
-    return arbitrage
+    # return arbitrage
 
 # Streamlit interface
 def main():
-    st.title('Crypto Arbitrage Trading App')
+    # st.title('Crypto Arbitrage Trading App')
 
-    # User inputs
-    base_coin = st.selectbox('Select Base Coin', ['btcusd', 'ethusd', 'ltcusd'])
-    investment_coin = st.selectbox('Select Investment Coin', ['btcusd', 'ethusd', 'ltcusd'])
+    # # User inputs
+    # base_coin = st.selectbox('Select Base Coin', ['btcusd', 'ethusd', 'ltcusd'])
+    # investment_coin = st.selectbox('Select Investment Coin', ['btcusd', 'ethusd', 'ltcusd'])
 
-    # Calculate arbitrage
-    arbitrage = calculate_arbitrage(base_coin, investment_coin)
+    # # Calculate arbitrage
+    # arbitrage = calculate_arbitrage(base_coin, investment_coin)
 
-    # Display results
-    st.write(f'The arbitrage between {base_coin} and {investment_coin} is {arbitrage}')
+    # # Display results
+    # st.write(f'The arbitrage between {base_coin} and {investment_coin} is {arbitrage}')
+    fetch_data()
 
 if __name__ == "__main__":
     main()
