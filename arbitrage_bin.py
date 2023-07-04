b12f68bac96bec0bc8819849908edfd85ca3367e
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-07-04 13:38:52 +0300

    Update arbitrage_bin.py

--- arbitrage_bin.py
+++ arbitrage_bin.py
@@ -1,51 +1,41 @@
-import os
 import streamlit as st
-from binance import Client
+import requests
+import pandas as pd
 
-api_key = st.secrets['BINANCE_API']
-api_secret = st.secrets['BINANCE_SECRET']
+# Fetch data from Tiingo API
+def fetch_data(coin):
+    url = f"https://api.tiingo.com/tiingo/crypto/prices?tickers={coin}"
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Token ' + st.secrets['TIINGO_API']
+    }
+    response = requests.request("GET", url, headers=headers)
+    data = response.json()
+    return data
 
-client = Client(api_key, api_secret)
+# Calculate arbitrage
+def calculate_arbitrage(base_coin, investment_coin):
+    # Fetch data for base and investment coins
+    base_data = fetch_data(base_coin)
+    investment_data = fetch_data(investment_coin)
 
-def get_available_coins():
-    exchange_info = client.get_exchange_info()
-    symbols = exchange_info['symbols']
-    coins = set()
+    # Calculate arbitrage (this is a simplified example, you'll need to implement your own logic here)
+    arbitrage = investment_data[0]['price'] - base_data[0price']
 
-    for symbol in symbols:
-        base_asset = symbol['baseAsset']
-        quote_asset = symbol['quoteAsset']
-        coins.add(base_asset)
-        coins.add(quote_asset)
+    return arbitrage
 
-    return sorted(coins)
+# Streamlit interfacedef main():
+    st.title('Crypto Arbitrage Trading App')
 
-def calculate_profit_margins(klines, price):
-    profit_margins = []
+    # User inputs
+    base_coin = st.selectbox('Select Base Coin', ['btcusd', 'ethusd', 'ltcusd'])
+    investment_coin = st.selectbox('Select Investment Coin', ['btcusd', 'ethusd', 'ltcusd'])
 
-    for kline in klines:
-        open_price = float(kline[1])
-        close_price = float(kline[4])
-        profit_margin = (close_price - open_price) / open_price * 100
-        profit_margins.append(profit_margin)
+    # Calculate arbitrage
+    arbitrage = calculate_arbitrage(base_coin, investment_coin)
 
-    return profit_margins
+    # Display results
+    st.write(f'The arbitrage between {base_coin} and {investment_coin} is {arbitrage}')
 
-def main():
-    st.title('Binance Arbitrage Trading')
-    base_coin = st.selectbox('Select the base coin:', get_available_coins())
-    investment_coin = st.selectbox('Select the investment coin:', get_available_coins())
-    interval = st.selectbox('Select the interval:', ['15m', '30m', '1h', '4h', '1d', '3d', '1M', '1Y'])
-
-    ticker = client.get_symbol_ticker(symbol=f"{base_coin}{investment_coin}")
-    price = float(ticker['price'])
-
-    klines = client.get_historical_klines(symbol=f"{base_coin}{investment_coin}", interval=interval)
-
-    profit_margins = calculate_profit_margins(klines, price)
-
-    st.write(f"Profit margins for {base_coin} to {investment_coin} using {interval} interval:")
-    st.write(profit_margins)
-
-if __name__ == '__main__':
+ifname == "__main__":
     main()
