57ef7c7388318a2aa9f9e5362ad54c7de6c332eb
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-07-17 15:41:42 +0300

    Update tech_analysis_st_app.py

--- fx/tech_analysis_st_app.py
+++ fx/tech_analysis_st_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import sys
-from technical_analysis import TechnicalAnalysis
 #sys.path.append('/app/business/fx')
 sys.path.append('/app/business')
+from technical_analysis import TechnicalAnalysis
 results=TechnicalAnalysis.technicalIndicators()
 st.write(results)
