20ecfb74f51a724ff71b6c8d4db045ea1d1edb06
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-07-18 05:45:16 +0300

    Update tech_analysis_st_app.py
    Line 7 updated module name

--- fx/tech_analysis_st_app.py
+++ fx/tech_analysis_st_app.py
@@ -4,9 +4,9 @@ import sys
 #from ta import trend
 #sys.path.append('/app/business/fx')
 sys.path.append('/app/business')
-from technical_analysis import TechnicalAnalysis
-ta=TechnicalAnalysis()
-results=ta.technicalIndicators()
+from technical_analysis import TIndicators
+ta=TIndicators
+results=ta.MACD()
 c=np.random.randn(100)
 #macd=trend.MACD(close=c,window_slow=26,window_fast=12,window_sign=9)
 st.write(results)
