880a92251cf74e18c4c5985949aee73de319b05a
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-07-18 14:38:15 +0300

    Update main.py
    Line 35 and 36 Period intervals.

--- fx/main.py
+++ fx/main.py
@@ -32,7 +32,8 @@ maj_forex_pairs = [
     "USDCAD",
     "NZDUSD"
 ]
-
+intervals = ['1d', '5d', '1mo', '3mo', '6mo', '1y', '2y', '5y', '10y', 'ytd', 'max']
+labels = ['1 Day', '5 Days', '1 Month', '3 Months', '6 Months','1 Year','2 Years','5 Years','10 Years','YTD','Max']
 import pandas as pd
 import yfinance as yf
 def collect_forex_data(currency_pair):
