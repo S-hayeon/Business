75c92c5469768c418b7c1f4dfc8bfaca4ab336a8
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-07-18 14:44:06 +0300

    Update tech_analysis_st_app.py

--- fx/tech_analysis_st_app.py
+++ fx/tech_analysis_st_app.py
@@ -11,7 +11,10 @@ from technical_analysis import TIndicators
 #macd=trend.MACD(close=c,window_slow=26,window_fast=12,window_sign=9)
 currencypair=st.selectbox("Select you investment currency pair: ",main.maj_forex_pairs)
 #st.write(results)
-data = main.collect_forex_data(f"{currencypair}=X")
+selected_interval = st.slider('Select an interval:', 0, len(main.intervals)-1, format_func=lambda i: main.labels[i])
+# Map the selected value to the corresponding interval
+selected_value = main.intervals[selected_interval]
+data = main.collect_forex_data(f"{currencypair}=X",selected_interval)
 ta=TIndicators(data['Open'])
 results=ta.MACD()
 st.write(results)
