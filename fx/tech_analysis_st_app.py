e5f12db0a591b943fce842bb1a51925457b0d485
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-07-18 14:19:38 +0300

    Update tech_analysis_st_app.py
    Line 12-17 now accepting real-time fx data for technical analysis

--- fx/tech_analysis_st_app.py
+++ fx/tech_analysis_st_app.py
@@ -3,15 +3,17 @@ import sys
 #sys.path.append('/app/business/fx')
 sys.path.append('/app/business')
 from fx import main
+import matplotlib.pyplot as plt
 import numpy as np
 import streamlit as st
 from technical_analysis import TIndicators
-ta=TIndicators()
-results=ta.MACD()
-c=np.random.randn(100)
+#c=np.random.randn(100)
 #macd=trend.MACD(close=c,window_slow=26,window_fast=12,window_sign=9)
 currencypair=st.selectbox("Select you investment currency pair: ",main.maj_forex_pairs)
 #st.write(results)
 data = main.collect_forex_data(f"{currencypair}=X")
-st.write(data)
-st.write(f"Yaani umechagua {currencypair}")
+ta=TIndicators(data)
+macd,signal=ta.MACD()
+st.write(results)
+#st.write(data)
+st.write(f"Investment yako inabamba enyewe {currencypair}")
