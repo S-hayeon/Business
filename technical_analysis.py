92fa70a56c5fb63358c0a7cebb9b435b189ea415
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-07-19 22:36:12 +0300

    Update technical_analysis.py
    Streamlit Matplotlib function st.pyplot

--- technical_analysis.py
+++ technical_analysis.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import streamlit as st
 import ta_py as ta
 class TIndicators:
   def __init__(self,data):
@@ -10,15 +11,34 @@ class TIndicators:
     length1 = 12; # default = 12
     length2 = 26; # default = 26
     length3= 9;
-    macd = ta.macd(self.data, length1, length2)
-    signal_line = ta.ema(self.data,length3)
-    plt.plot(macd, label='MACD Line')
-    plt.plot(signal_line, label='Signal Line')
-    plt.xlabel('Time')
-    plt.ylabel('Value')
-    plt.title('MACD and Signal Line')
-    plt.legend()
+    # macd = ta.macd(self.data, length1, length2)
+    # signal_line = ta.ema(self.data,length3)
+    # plt.plot(macd, label='MACD Line')
+    # plt.plot(signal_line, label='Signal Line')
+    # plt.xlabel('Time')
+    # plt.ylabel('Value')
+    # plt.title('MACD and Signal Line')
+    # plt.legend()
+    # plt.show()
+    
+    # Create a new figure and axis
+    fig, ax = plt.subplots()
+
+    # Plot MACD Line and Signal Line on the same axis
+    ax.plot(macd, label='MACD Line')
+    ax.plot(signal_line, label='Signal Line')
+
+    # Set axis labels and title
+    ax.set_xlabel('Time')
+    ax.set_ylabel('Value')
+    ax.set_title('MACD and Signal Line')
+
+    # Show legend
+    ax.legend()
+
+    # Show the plot
     plt.show()
+    st.pyplot(fig)
 
     # Check for crossovers between MACD and Signal Line
     for i in range(1, len(macd)):
