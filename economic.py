139a85ab24c36d857e407578ed536922bac6be2d
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-07-20 13:48:12 +0300

    Update economic.py
    Interest and Inflation functions adjusted

--- economic.py
+++ economic.py
@@ -8,17 +8,32 @@ class Economic:
     #Appproach 2:Indicator: FR.INR.RINR
     #Details Real interest rate (%)
     #wb.get_series('FR.INR.RINR')
-    series=pd.DataFrame(wb.get_series('FR.INR.RINR',date='2000:2023'))
+    # Fetch the data as a Series
+    series = wb.get_series('FR.INR.RINR', date='2000:2023')
+    # Convert the Series to a DataFrame
+    df = pd.DataFrame({'Value': series})
+    # Reset the index to move 'Country' and 'Year' from the MultiIndex to regular columns
+    df = df.reset_index()
     # Filter the DataFrame based on the specified country
+    interestDF = df[df['Country'] == str(Country)]
+    # Reset the index to remove the existing index and create a new default integer index
+    interestDF = interestDF.reset_index(drop=True)
     interestDF = series[series['Country'] == str(Country)]
     return interestDF
   def inflation(self,Country):
     #Indicator: FP.CPI.TOTL.ZG 
     #Details Inflation, consumer prices (annual %)
     #wb.get_series('FP.CPI.TOTL.ZG')
-    series=pd.DataFrame(wb.get_series('FP.CPI.TOTL.ZG',date='2000:2023'))
+    series=wb.get_series('FP.CPI.TOTL.ZG',date='2000:2023')
+    # Convert the Series to a DataFrame
+    df = pd.DataFrame({'Value': series})
+    # Reset the index to move 'Country' and 'Year' from the MultiIndex to regular columns
+    df = df.reset_index()
     # Filter the DataFrame based on the specified country
-    inflationDF = series[series['Country'] == str(Country)]
+    inflationDF = df[df['Country'] == str(Country)]
+    # Reset the index to remove the existing index and create a new default integer index
+    inflationDF=interestDF.reset_index(drop=True)
+    inflationDF=series[series['Country'] == str(Country)]
     return inflationDF
       
       
