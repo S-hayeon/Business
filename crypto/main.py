d9ad545441ebc547f7ed2404b95ad61469fb29f6
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-07-24 08:47:02 +0300

    Create main.py
    Coins and Tokens Dictionaries

---
+++ crypto/main.py
@@ -0,0 +1,25 @@
+crypto_coins = {
+    "stable_coins": ["USDT", "USDC", "DAI", "BUSD"],
+    "fiat_currency": ["USD", "EUR"],
+    "popular_coins": ["BTC", "ETH", "BNB"]
+}
+crypto_tokens = {
+    "Storage coins": ["FIL", "STORJ", "SC"],
+    "Meme": ["DOGE", "SHIB"],
+    "AI": ["AGI", "FET"],
+    "Metaverse": ["MANA", "SAND"],
+    "Infrastructure": ["LINK", "BAND"],
+    "Fan Token": ["CHZ", "SOC"],
+    "Launchpad": ["BNB", "POLS"],
+    "POW": ["BTC", "ETH", "LTC"],
+    "POS": ["ADA", "XTZ", "ATOM"],
+    "Layer 1/ Layer 2": ["ETH", "MATIC", "SOL"],
+    "BNB Chain": ["BNB"],
+    "NFT": ["PUNK", "BAYC"],
+    "Gaming": ["ENJ", "AXS"],
+    "Innovation": ["ADA", "DOT"],
+    "Launchpool": ["BNB"],
+    "DeFi": ["UNI", "COMP", "AAVE"],
+    "ETF": ["CBI"],
+    "Polkadot": ["DOT"],
+}
