79ba238a9db4c24ea5fc80a582483e50d8ff22f3
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-07-25 15:45:23 +0300

    Update chart_patterns.py
    Support and Resistance levels

--- Technical_Analysis/chart_patterns.py
+++ Technical_Analysis/chart_patterns.py
@@ -1,2 +1,15 @@
 # Patterns
-
+import ta_py as ta
+class Pattern:
+    def __init__(self, data):
+        self.data = data
+    def support_resistance(self):
+        # Calculate support and resistance levels using ta-py module
+        lookback = 15; # No lower values after 4 periods? resets after each new low
+        recent_low=ta.recent_low(self.data['Low'],lookback)
+        lookback = 15; # No lower values after 4 periods? resets after each new low
+        recent_high=ta.recent_high(self.data['High'],lookback)
+        self.support = ta.support(self.data['Low'],recent_low)
+        #self.data['resistance'] = ta.volatility.resistance(self.data['Close'], n=10, percentage=3)
+        self.resistance = ta.resistance(self.data['High'],recent_high)
+        return self.support['lowest'],self.resistance['highest']
