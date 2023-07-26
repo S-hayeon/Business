11c89c81dc5465191c25e31403c859e3125534cc
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-07-26 15:39:58 +0300

    Update chart_patterns.py

--- Technical_Analysis/chart_patterns.py
+++ Technical_Analysis/chart_patterns.py
@@ -1,15 +1,18 @@
-# Patterns
 import ta_py as ta
 class Pattern:
     def __init__(self, data):
         self.data = data
-    def support_resistance(self):
+
+    def support_resistance(self, lookback=None):
+        # If lookback is None, use the default value of 15
+        if lookback is None:
+            lookback = 15
+
         # Calculate support and resistance levels using ta-py module
-        lookback = 15; # No lower values after 4 periods? resets after each new low
-        recent_low=ta.recent_low(self.data['Low'],lookback)
-        lookback = 15; # No lower values after 4 periods? resets after each new low
-        recent_high=ta.recent_high(self.data['High'],lookback)
-        self.support = ta.support(self.data['Low'],recent_low)
-        #self.data['resistance'] = ta.volatility.resistance(self.data['Close'], n=10, percentage=3)
-        self.resistance = ta.resistance(self.data['High'],recent_high)
-        return self.support['lowest'],self.resistance['highest']
+        recent_low = ta.recent_low(self.data['Low'], lookback)
+        recent_high = ta.recent_high(self.data['High'], lookback)
+
+        self.support = ta.support(self.data['Low'], recent_low)
+        self.resistance = ta.resistance(self.data['High'], recent_high)
+
+        return self.support['lowest'], self.resistance['highest']
