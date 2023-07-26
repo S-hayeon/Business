a656645567f89292c43ae70ef31ea151dcaad5f4
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-07-26 16:56:33 +0300

    Update Chart_Patterns.py
    Line 37-39 updated.

--- crypto/pages/Chart_Patterns.py
+++ crypto/pages/Chart_Patterns.py
@@ -32,14 +32,15 @@ if st.session_state['CurrencyPair'] is not None and st.session_state['DataFrame'
             support_resistance_lines=list(chart_pattern.support_resistance(int(lookback)))
             fig=mpf.plot(st.session_state['DataFrame'],type='candle',volume=True,style='binance',hlines=dict(hlines=support_resistance_lines,colors=['g','r'],linestyle='-.'))
             candlestickfigure_placeholder.pyplot(fig)
-        with st.expander("More info"):
-            st.info("Sensitivity is the % of data the system looks back to find support and resistance.")
-            st.write(f"Support Level in Green: {support_resistance_lines[0]}")
-            st.write(f"Resistance Level in Red: {support_resistance_lines[1]}")
                          
-    st.header(":green[Support] and red[Resistance] Levels")
-    support_Resistance()
     st.set_option('deprecation.showPyplotGlobalUse', False)
+    st.header(":green[Support] and :red[Resistance] Levels")
+    support_Resistance()
+    with st.expander("More info on Support and Resistance"):
+        st.info("Sensitivity is the % of data the system looks back to find support and resistance.")
+        # Access support_resistance_lines from st.session_state
+        st.write(f"Support Level in Green: {st.session_state.support_resistance_lines[0]}")
+        st.write(f"Resistance Level in Red: {st.session_state.support_resistance_lines[1]}")
 else:
     st.warning("Choose your desired coin from the CryptoAnalysis page to proceed!!")
 
