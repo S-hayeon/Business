01b52fb8f22a3c625fe92b6a181ae14a38efdfa4
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-07-26 16:14:11 +0300

    Update Chart_Patterns.py
    Expander plus with added functionalities

--- crypto/pages/Chart_Patterns.py
+++ crypto/pages/Chart_Patterns.py
@@ -16,25 +16,31 @@ except:
 #sys.path.append('/app/business/fx')
 import time
 
-try:
-    if st.session_state['CurrencyPair'] is not None and st.session_state['DataFrame'] is not None:
-        def support_Resistance():
-            # Create a placeholder for the dataframe
-            data_placeholder = st.empty()
-            candlestickfigure_placeholder = st.empty()
-            status_displayed = False  # Flag to track whether status message has been displayed
-            # Continuously update the data by fetching new data from the API
-            while True:
-                data_placeholder.dataframe(st.session_state['DataFrame'])
-                # Display status message only once
-                chart_pattern=chart_patterns.Pattern(data=st.session_state['DataFrame'])
-                support_resistance_lines=list(chart_pattern.support_resistance())
-                fig=mpf.plot(st.session_state['DataFrame'],type='candle',volume=True,style='binance',hlines=dict(hlines=support_resistance_lines,colors=['g','r'],linestyle='-.'))
-                candlestickfigure_placeholder.pyplot(fig)
-        support_Resistance()
-        st.set_option('deprecation.showPyplotGlobalUse', False)
-    else:
-        st.warning("Choose your desired coin from the CryptoAnalysis page to proceed!!")
-except Exception as e:
-    st.info(f"System encountered this issue: {e}")
+
+if st.session_state['CurrencyPair'] is not None and st.session_state['DataFrame'] is not None:
+    def support_Resistance():
+        # Create a placeholder for the dataframe
+        data_placeholder = st.empty()
+        candlestickfigure_placeholder = st.empty()
+        status_displayed = False  # Flag to track whether status message has been displayed
+        # Continuously update the data by fetching new data from the API
+        lookback=st.slider(label="Sensitivity in Percentage %", min_value=1, max_value=100, value=15, step=1)
+        while True:
+            data_placeholder.dataframe(st.session_state['DataFrame'])
+            # Display status message only once
+            chart_pattern=chart_patterns.Pattern(data=st.session_state['DataFrame'])
+            support_resistance_lines=list(chart_pattern.support_resistance(int(lookback)))
+            fig=mpf.plot(st.session_state['DataFrame'],type='candle',volume=True,style='binance',hlines=dict(hlines=support_resistance_lines,colors=['g','r'],linestyle='-.'))
+            candlestickfigure_placeholder.pyplot(fig)
+        with st.expander("More info"):
+            st.info("Sensitivity is the % of data the system looks back to find support and resistance.")
+            st.write(f"Support Level in Green: {support_resistance_lines[0]}")
+            st.write(f"Resistance Level in Red: {support_resistance_lines[1]}")
+                         
+    st.header(":green[Support] and red[Resistance] Levels")
+    support_Resistance()
+    st.set_option('deprecation.showPyplotGlobalUse', False)
+else:
+    st.warning("Choose your desired coin from the CryptoAnalysis page to proceed!!")
+
 
