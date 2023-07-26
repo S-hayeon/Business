a6e30aac052d5093357ddfd92a359b7a3c11143f
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-07-26 16:22:46 +0300

    Update chart_patterns.py

--- Technical_Analysis/chart_patterns.py
+++ Technical_Analysis/chart_patterns.py
@@ -1,3 +1,4 @@
+
 import ta_py as ta
 class Pattern:
     def __init__(self, data):
@@ -5,12 +6,13 @@ class Pattern:
 
     def support_resistance(self, lookback=None):
         # If lookback is None, use the default value of 15
+        dataframeLength=len(self.data)
         if lookback is None:
-            lookback = 15
-
+            lookback = 25
+        lookbackPeriod=(lookback/100)*dataframeLength
         # Calculate support and resistance levels using ta-py module
-        recent_low = ta.recent_low(self.data['Low'], lookback)
-        recent_high = ta.recent_high(self.data['High'], lookback)
+        recent_low = ta.recent_low(self.data['Low'], lookbackPeriod)
+        recent_high = ta.recent_high(self.data['High'],lookbackPeriod)
 
         self.support = ta.support(self.data['Low'], recent_low)
         self.resistance = ta.resistance(self.data['High'], recent_high)
