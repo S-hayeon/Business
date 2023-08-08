8f66ad661efd272016226e668add5be826523b94
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-08-08 12:26:04 +0300

    Update Real_Time_Data.py
    Line 1-19 Updated with a new websocket in Line 6

--- crypto/Real_Time_Data.py
+++ crypto/Real_Time_Data.py
@@ -1,34 +1,19 @@
 import streamlit as st
 import websocket
-import threading
-import json
+info_placeholder=st.empty()
+error_placeholder=st.empty()
+text_placeholder=st.empty()
+SOCKET="wss://stream.binance.com:443/ws/ethusdt@kline_1m"
+def on_open(ws):
+  info_placeholder.info("Nimefungua data stream")
+def on_error(ws, error):
+    error_placeholder.error(error)
+def on_close(ws):
+  info_placeholder.info("Nimefunga data stream")
+def on_message(ws,message):
+  info_placeholder.info("Data stream sasa inaFlow")
+  text_placeholder.text(message)
 
-# Streamlit configuration
-st.set_page_config(page_title="Binance WebSocket App", layout="wide")
-
-# Initialize Streamlit components
-st.title("Binance WebSocket App")
-trade_data = st.empty()
-
-# WebSocket callback function
-def on_message(ws, message):
-    data = json.loads(message)
-    trade_data.text(f"Price: {data['p']} | Quantity: {data['q']}")
-
-# Streamlit UI
-st.sidebar.header("Settings")
-symbol = st.sidebar.text_input("Enter Symbol (e.g., btcusdt)", "btcusdt")
-binance_ws_endpoint = f"wss://stream.binance.com:9443/ws/{symbol}@trade"
-
-if symbol:
-    st.sidebar.write("WebSocket URL:", binance_ws_endpoint)
-
-    def run_ws():
-        ws = websocket.WebSocketApp(binance_ws_endpoint, on_message=on_message)
-        ws.run_forever()
-
-    connect_button = st.sidebar.button("Connect")
-    
-    if connect_button:
-        thread = threading.Thread(target=run_ws)
-        thread.start()
+st.title("Crypto Live Datastream")
+ws=websocket.WebSocketApp(SOCKET,on_open=on_open,on_close=on_close,on_message=on_message)
+ws.run_forever() #Loop the streaming functionality
