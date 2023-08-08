2e9f8f5fff7162f1229c8bb71648b91987aa4a4a
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-08-08 12:06:08 +0300

    Update Real_Time_Data.py

--- crypto/Real_Time_Data.py
+++ crypto/Real_Time_Data.py
@@ -1,6 +1,7 @@
-import json
 import streamlit as st
 import websocket
+import threading
+import json
 
 # Streamlit configuration
 st.set_page_config(page_title="Binance WebSocket App", layout="wide")
@@ -17,13 +18,17 @@ def on_message(ws, message):
 # Streamlit UI
 st.sidebar.header("Settings")
 symbol = st.sidebar.text_input("Enter Symbol (e.g., btcusdt)", "btcusdt")
-binance_ws_endpoint = f"wss://stream.binance.com:9443/ws/{symbol}@kline_1s"
+binance_ws_endpoint = f"wss://stream.binance.com:9443/ws/{symbol}@trade"
 
 if symbol:
     st.sidebar.write("WebSocket URL:", binance_ws_endpoint)
-    ws = websocket.WebSocketApp(binance_ws_endpoint, on_message=on_message)
 
     def run_ws():
+        ws = websocket.WebSocketApp(binance_ws_endpoint, on_message=on_message)
         ws.run_forever()
 
-    st.sidebar.button("Connect", on_click=run_ws)
+    connect_button = st.sidebar.button("Connect")
+    
+    if connect_button:
+        thread = threading.Thread(target=run_ws)
+        thread.start()
