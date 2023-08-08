e93ca1aa4516d58110801124b137750a5019de95
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-08-08 11:14:45 +0300

    Update real_time_data.py

--- crypto/real_time_data.py
+++ crypto/real_time_data.py
@@ -9,8 +9,8 @@ def on_open(ws):
 def on_close(ws):
   error_placeholder.error("Nimefunga data stream")
 def on_message(ws,message):
-  info_placeholder.info("Data stream inaFlow")
-  text_placeholder.write(f"Kuna hii data:{message}")
+  st.info("Data stream inaFlow")
+  st.write(f"Kuna hii data:{message}")
 
 ws=websocket.WebSocketApp(SOCKET,on_open=on_open,on_close=on_close,on_message=on_message)
 ws.run_forever() #Loop the streaming functionality
