53ecf23ba5ca3ca5b92d4799eed5cad7f64ebfba
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-08-08 18:45:02 +0300

    Update real_time_data.py
    Update real_time_data

--- crypto/real_time_data.py
+++ crypto/real_time_data.py
@@ -1,17 +1,30 @@
 import streamlit as st
 import websocket
-info_placeholder=st.empty()
-error_placeholder=st.empty()
-text_placeholder=st.empty()
-SOCKET = "wss://stream.binance.com:443/ws/ethusdt@kline_1m"
-def on_open(ws):
-  st.info("Nimefungua data stream")
-def on_close(ws):
-  st.error("Nimefunga data stream")
-def on_message(ws,message):
-  st.info("Data stream inaFlow")
-  print(f"Kuna hii data:{message}")
 
-st.info("Running the Crypto Data stream")
-ws = websocket.WebSocketApp(SOCKET, on_open=on_open, on_close=on_close, on_message=on_message)
-ws.run_forever() #Loop the streaming functionality
+info_placeholder = st.empty()
+error_placeholder = st.empty()
+text_placeholder = st.empty()
+SOCKET = "wss://stream.binance.com:443/ws/ethusdt@kline_1m"
+ws = None  # Initialize the WebSocket instance
+
+def on_open(ws):
+    info_placeholder.info("Nimefungua data stream")
+
+def on_close(ws):
+    info_placeholder.info("Nimefunga data stream")
+
+def on_message(ws, message):
+    st.info("Data stream sasa inaFlow")
+    st.text(message)
+
+st.title("Crypto Live Datastream")
+
+# Create a button to start the WebSocket connection
+if st.button("Start Data Stream"):
+    def start_stream():
+        global ws
+        ws = websocket.WebSocketApp(
+            SOCKET, on_open=on_open, on_close=on_close, on_message=on_message)
+        ws.run_forever()
+    
+    start_stream()
