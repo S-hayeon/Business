7eace6b33a6eab1259b67af15db42a18ae0d2921
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-08-08 11:11:25 +0300

    Update real_time_data.py
    Create empty placeholder

--- crypto/real_time_data.py
+++ crypto/real_time_data.py
@@ -1,13 +1,15 @@
 import streamlit as st
 import websocket
+info_placeholder=st.empty()
+error_placeholder=st.empty()
 text_placeholder=st.empty()
 SOCKET="wss://stream.binance.com:9443/ethusdt@kline_1m"
 def on_open(ws):
-  text_placeholder.info("Nimefungua data stream")
+  info_placeholder.info("Nimefungua data stream")
 def on_close(ws):
-  text_placeholder.error("Nimefunga data stream")
+  error_placeholder.error("Nimefunga data stream")
 def on_message(ws,message):
-  text_placeholder.info("Data stream inaFlow")
+  info_placeholder.info("Data stream inaFlow")
   text_placeholder.write(f"Kuna hii data:{message}")
 
 ws=websocket.WebSocketApp(SOCKET,on_open=on_open,on_close=on_close,on_message=on_message)
