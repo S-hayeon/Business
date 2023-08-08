43aa4747f23d79d367ee3ce3dac7e5a6d40b319a
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-08-08 11:33:09 +0300

    Update real_time_data.py
    Line 2-17

--- crypto/real_time_data.py
+++ crypto/real_time_data.py
@@ -3,15 +3,15 @@ import websocket
 info_placeholder=st.empty()
 error_placeholder=st.empty()
 text_placeholder=st.empty()
-SOCKET="wss://stream.binance.com:9443/ws/ethusdt@kline_1m"
+SOCKET = "wss://stream.binance.com:443/ws/ethusdt@kline_1m"
 def on_open(ws):
-  info_placeholder.info("Nimefungua data stream")
+  st.info("Nimefungua data stream")
 def on_close(ws):
-  error_placeholder.error("Nimefunga data stream")
+  st.error("Nimefunga data stream")
 def on_message(ws,message):
   st.info("Data stream inaFlow")
   print(f"Kuna hii data:{message}")
 
 st.info("Running the Crypto Data stream")
-ws=websocket.WebSocketApp(SOCKET,on_open=on_open,on_close=on_close,on_message=on_message)
+ws = websocket.WebSocketApp(SOCKET, on_open=on_open, on_close=on_close, on_message=on_message)
 ws.run_forever() #Loop the streaming functionality
