9f2a28e1a2f7d813b1fe4a0814fcac86f5cd7c80
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-08-08 10:59:47 +0300

    Create real_time_data.py
    Crypto prices live stream.

---
+++ crypto/real_time_data.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import websocket
+text_placeholder=st.empty()
+SOCKET="wss://stream.binance.com:9443/ethusdt@kline_1m"
+def on_open(ws):
+  text_placeholder.info("Nimefungua data stream")
+def on_close(ws):
+  text_placeholder.error("Nimefunga data stream")
+def on_message(ws,message):
+  text_placeholder.info("Data stream inaFlow")
+  text_placeholder.write(f"Kuna hii data:{message}")
+
+ws=websocket.WebsocketApp(SOCKET,on_open=on_open,on_close=on_close,on_message=on_message)
+ws.run_forever() #Loop the streaming functionality
