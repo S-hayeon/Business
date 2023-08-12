f498a75bbe6033ea5ce0b76bb29fae4ff380b833
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-08-12 13:05:46 +0300

    Update chart_patterns.py
    Include candlestick identification function.

--- Technical_Analysis/chart_patterns.py
+++ Technical_Analysis/chart_patterns.py
@@ -20,3 +20,52 @@ class Pattern:
 
         #return self.support['lowest'], self.resistance['highest']
         return self.support['calculate'](len(self.data)-self.support['index']), self.resistance['calculate'](len(self.data)-self.resistance['index'])
+    def candlestick_Pattern(self,index):
+        self.index=index
+        """
+        Identifies the candlestick pattern at the specified self.index using talib.
+        Identifies the candlestick trend at the specified self.index using talib.
+    
+        Parameters:
+            self.data (pd.DataFrame): A DataFrame containing OHLC (Open, High, Low, Close) self.data.
+            self.index (int): The self.index at which to identify the candlestick pattern.
+    
+        Returns:
+            str: The identified candlestick pattern, or 'None' if no pattern is found.
+        """
+        if self.index >= len(self.data) or self.index < 0:
+            return "Invalid self.index"
+    
+        # Extract OHLC self.data from DataFrame
+        open_prices = self.data['Open'].values.astype(float)
+        high_prices = self.data['High'].values.astype(float)
+        low_prices = self.data['Low'].values.astype(float)
+        close_prices = self.data['Close'].values.astype(float)
+    
+        # Make sure we have enough self.data to compute the candlestick pattern
+        if len(open_prices) < 2:
+            return "Not enough self.data to identify the candlestick pattern."
+    
+        # Calculate the candlestick pattern using talib
+        patterns = talib.get_function_groups()['Pattern Recognition']
+        #bullish_patterns=['CDLHAMMER','CDLINVERTEDHAMMER','CDLMORNINGSTAR','CDLDRAGONFLYDOJI']
+        result = {}
+        for pattern in patterns:
+            pattern_function = getattr(talib, pattern)
+            result[pattern] = pattern_function(open_prices, high_prices, low_prices, close_prices)
+    
+        # Get the pattern at the specified self.index
+        pattern_at_index = {}
+        for pattern, values in result.items():
+            if values[self.index] != 0:
+                pattern_at_index[pattern] = values[self.index]
+    
+        # Return the identified pattern
+        if pattern_at_index:
+            #print(pattern_at_index)
+            self.max_pattern = max(pattern_at_index, key=pattern_at_index.get) # Most relevant or significant pattern among the ones identified
+            #self.max_pattern = pattern_at_index[0]
+            #print(self.max_pattern)
+            self.max_value = pattern_at_index[self.max_pattern] #Strength of the respective candlestick pattern. Positive: 100, Negative:-100, Uncertain:0
+            return [max_pattern,max_value]
+            #return self.max_pattern
