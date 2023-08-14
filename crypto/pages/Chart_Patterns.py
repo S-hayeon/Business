9e0ea3421723bbfaa97df15fc15aabe5c2d3b635
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-08-14 08:20:00 +0300

    Update Chart_Patterns.py
    Line 60-63 Pattern and Trend

--- crypto/pages/Chart_Patterns.py
+++ crypto/pages/Chart_Patterns.py
@@ -56,8 +56,10 @@ if st.session_state['CurrencyPair'] is not None and st.session_state['DataFrame'
     for dfindex in range(len(st.session_state['DataFrame'])):
         patternsID=candlestickID.candlestick_Pattern(dfindex)
         #candlestickDF.at[dfindex,'Date']=st.session_state['DataFrame'].iloc[dfindex]['Date']
-        candlestickDF.at[dfindex,'Pattern']=patternsID[0]
-        candlestickDF.at[dfindex,'Trend']=patternsID[1]
+        candlestickDF.at[dfindex,'Pattern']=patternsID.max_pattern
+        #candlestickDF.at[dfindex,'Pattern']=patternsID[0]
+        #candlestickDF.at[dfindex,'Value']=patternsID.max_value
+        candlestickDF.at[dfindex,'Trend']=patternsID.trend
         candlestickpatterns_placeholder = st.empty() # Create a placeholder for the candlestick Patterns Dataframe 
     with st.expander("View Candlestick Patterns"):
             candlestickpatterns_placeholder.dataframe(candlestickDF)
