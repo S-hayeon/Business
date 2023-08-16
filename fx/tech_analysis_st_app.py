52b1d2b16354189b6fb91088e609fad11b4624eb
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-08-16 11:35:02 +0300

    Update tech_analysis_st_app.py
    Sidebar feature added. Historical Data Table

--- fx/tech_analysis_st_app.py
+++ fx/tech_analysis_st_app.py
@@ -22,31 +22,50 @@ import time
 import yfinance as yf
 #c=np.random.randn(100)
 #macd=trend.MACD(close=c,window_slow=26,window_fast=12,window_sign=9)
+################ Session State Variables ###############################################################################################################
 if "CurrencyPair" not in st.session_state:
   st.session_state["CurrencyPair"]=""
 if "Symbol" not in st.session_state:
   st.session_state["Symbol"]=""
 if "Interval" not in st.session_state:
   st.session_state["Interval"]=""
-currencypair=st.selectbox("Select you investment currency pair: ",main.maj_forex_pairs)
+if "StartDate" not in st.session_state:
+  st.session_state["StartDate"]=""
+if "EndDate" not in st.session_state:
+  st.session_state["EndDate"]=""
+
+################ Sidebar ###############################################################################################################
+currencypair=st.sidebar.selectbox("Select you investment currency pair: ",main.maj_forex_pairs)
 st.session_state["CurrencyPair"]=currencypair
 cur_symbol=f"{currencypair}=X"
 st.session_state["Symbol"]=cur_symbol
 ticker=yf.Ticker(st.session_state["Symbol"])
 # Create the select slider
-selected_interval = st.select_slider('Select an interval:', options=main.labels)
+selected_interval = st.sidebar.select_slider('Select an interval:', options=main.labels)
 # Map the selected value to the corresponding interval
 #selected_value = main.intervals[selected_interval]
 # Map the selected label to the corresponding interval
 selected_value = main.intervals[main.labels.index(selected_interval)]
 st.session_state["Interval"]=selected_value
 #data = main.collect_forex_data(f"{currencypair}=X",selected_value)
+start_date = st.sidebar.date_input("Start Date", pd.to_datetime("2023-01-01"))
+st.session_state("StartDate")=start_date
+end_date = st.sidebar.date_input("End Date", pd.to_datetime("2023-08-01"))
+st.session_state("EndDate")=end_date
+##########################################################################################################################################
+
+################ Ticker Information ###############################################################################################################
 tickerInfo=pd.DataFrame(ticker.info.items(),columns=['Parameter','Value'])
 tickerInfo[tickerInfo['Value']!=0].dropna() # Drop values where Value Column is equal to 0 then drop Nan/None values
 tickerInfo_expander=st.empty()
 with tickerInfo_expander.expander("FX Asset Information"):
   st.dataframe(tickerInfo)
 st.write(f"Investment yako ya {currencypair} inabamba enyewe")
+################ Historical and Real Time Data Table ###############################################################################################################
+data=ticker.history(interval=st.session_state["Interval"],start=start_date, end=end_date)
+tickerData_expander=st.empty()
+with tickerData_expander.expander(" OHLC Candlestick Fx Data"):
+  st.dataframe(data.dropna())
 #ta=TIndicators(data['Open'])
 #results=ta.MACD()
 #st.write(results)
