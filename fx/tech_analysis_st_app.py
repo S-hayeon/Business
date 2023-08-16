95d66107f16665943ed80282dd58bb2efe4b2d19
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-08-16 10:44:53 +0300

    Update tech_analysis_st_app.py
    MetaTrader meta data removed.

--- fx/tech_analysis_st_app.py
+++ fx/tech_analysis_st_app.py
@@ -2,9 +2,6 @@
 import datetime
 import matplotlib.pyplot as plt
 import numpy as np
-import os
-#/home/adminuser/venv/bin/python -m pip install --upgrade pip
-os.system("bash /mount/src/business/fx/setup.sh")
 #import MetaTrader5 as mt5
 import mt5
 import pandas as pd
@@ -28,28 +25,8 @@ if "CurrencyPair" not in st.session_state:
   st.session_state["CurrencyPair"]=""
 currencypair=st.selectbox("Select you investment currency pair: ",main.maj_forex_pairs)
 st.session_state["CurrencyPair"]=currencypair
-import mt5
-# Create a connection to the MT5 server
-mt5.initialize()
-fetchDataMode=st.selectbox("Select Data Fetch: ",['Historical','Real-Time'])
-if fetchDataMode=='Historical':
-  ticks = mt5.copy_ticks_from(currencypair, mt5.TIMEFRAME_D1, datetime(2023, 1, 1), datetime(2023, 7, 31)) # Get the historical tick data for the EURUSD symbol from 2023-01-01 to 2023-07-31
-elif fetchDataMode=='Real-Time':
-  #ticks=mt5.subscribe_to_market_data(currencypair) # Subscribe to the real time ticks data for the EURUSD symbol
-  # Get the latest tick data
-  ticks = mt5.get_last_tick("EURUSD")
-  # Print the tick data
-  #time.sleep(1)
-else:
-  pass
-ticks=pd.DataFrame(ticks)
-st.dataframe(ticks)
-#st.write(results)
-#selected_interval = st.slider('Select an interval:', 0, len(main.intervals)-1, format_func=lambda i: main.labels[i])
-
 # Create the select slider
 selected_interval = st.select_slider('Select an interval:', options=main.labels)
-
 # Map the selected value to the corresponding interval
 #selected_value = main.intervals[selected_interval]
 # Map the selected label to the corresponding interval
