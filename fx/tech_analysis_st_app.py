65dd9dc533101883478b8d1aaca454275043aac7
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-08-16 11:00:50 +0300

    Update tech_analysis_st_app.py
    Main app with ticker info. Technical analysis partially ignored for pages feature

--- fx/tech_analysis_st_app.py
+++ fx/tech_analysis_st_app.py
@@ -19,22 +19,33 @@ except:
 
 from technical_analysis import TIndicators
 import time
+import yfinance as yf
 #c=np.random.randn(100)
 #macd=trend.MACD(close=c,window_slow=26,window_fast=12,window_sign=9)
 if "CurrencyPair" not in st.session_state:
   st.session_state["CurrencyPair"]=""
+if "Symbol" not in st.session_state:
+  st.session_state["Symbol"]=""
+if "Interval" not in st.session_state:
+  st.session_state["Interval"]=""
 currencypair=st.selectbox("Select you investment currency pair: ",main.maj_forex_pairs)
 st.session_state["CurrencyPair"]=currencypair
+cur_symbol=f"{currencypair}=X"
+st.session_state["Symbol"]=cur_symbol
+ticker=yf.Ticker(st.session_state["Symbol"])
 # Create the select slider
 selected_interval = st.select_slider('Select an interval:', options=main.labels)
 # Map the selected value to the corresponding interval
 #selected_value = main.intervals[selected_interval]
 # Map the selected label to the corresponding interval
 selected_value = main.intervals[main.labels.index(selected_interval)]
-data = main.collect_forex_data(f"{currencypair}=X",selected_value)
-#data = main.collect_forex_data(f"{currencypair}=X",selected_interval)
-ta=TIndicators(data['Open'])
-results=ta.MACD()
-st.write(results)
+st.session_state["Interval"]=selected_value
+#data = main.collect_forex_data(f"{currencypair}=X",selected_value)
+tickerInfo=pd.DataFrame(ticker.info)
+st.dataframe(tickerInfo)
+st.write(f"Investment yako ya {currencypair} inabamba enyewe")
+#ta=TIndicators(data['Open'])
+#results=ta.MACD()
+#st.write(results)
 #st.write(data)
-st.write(f"Investment yako inabamba enyewe {currencypair}")
+
