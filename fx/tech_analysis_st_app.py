0ef91ebcff199a0b673fbf88932a7e9d5c30473c
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-08-16 12:11:45 +0300

    Update tech_analysis_st_app.py
    Reset index line changed

--- fx/tech_analysis_st_app.py
+++ fx/tech_analysis_st_app.py
@@ -63,7 +63,7 @@ with tickerInfo_expander.expander("FX Asset Information"):
 st.write(f"Investment yako ya {currencypair} inabamba enyewe")
 ################ Historical and Real Time Data Table ###############################################################################################################
 data=ticker.history(interval=st.session_state["Interval"],start=start_date, end=end_date)
-data=data.reset_index() # Reset Datetime index
+#data=data.reset_index() # Reset Datetime index
 #data=data[data['Open','High','Low','Close']]
 timezone=st.sidebar.selectbox("Select your Timezone: ",main.timezones)
 data=data.index.tz_convert(timezone)
