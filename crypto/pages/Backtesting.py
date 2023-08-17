4a55da074add66f0fedc1dd9beee3b4af0beadd1
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-08-17 09:35:03 +0300

    Create Backtesting.py
    Basic RSI Strategy for testing.

---
+++ crypto/pages/Backtesting.py
@@ -0,0 +1,31 @@
+from backtesting import Backtest,Strategy
+from backtesting.lib import crossover,plot_heatmaps
+import pandas as pd
+import streamlit as st
+import talib
+class MyStrategy(Strategy):
+  upper_bound=st.sidebar.slider("Enter the Indicator Upper Limit",0, 100)
+  lower_bound=st.sidebar.slider("Enter the Indicator Lower Limit",0, 100)
+  def init(self):
+    self.indicator1=self.I(talib.RSI,self.data.Close,self.time_period)
+  def next(self):
+    if self.indicator[-1]>self.upper_bound:
+      self.position.close()
+    elif self.indicator[-1]<self.lower_bound:
+      self.buy()
+cash=st.sidebar.slider("Enter you available cash",0,1000000,step=1)
+if st.button("Test my strategy"):
+  if st.session_state["DataFrame"] !=None:
+    bt=Backtest(st.session_state["DataFrame"],MyStrategy,cash)
+    strategyStats=bt.run()
+    strategyStatsDF=pd.DataFrame(strategyStats)
+    strategyTradesDF=pd.DataFrame(strategyStats['_trades'])
+    with st.container():
+      strategyStats_placeholder=st.empty()
+      strategyTrades_placeholder=st.empty()
+      with strategyStats_placeholder.expander("View Stategy Statistics"):
+        st.dataframe(strategyStatsDF)
+      with strategyTrades_placeholder.expander("View Strategy Trades"):
+        st.dataframe(strategyTradesDF)
+      
+      
