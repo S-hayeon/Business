133f2b92d1bc1cefe589cf7cc75fd0f7992ae720
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-08-18 06:48:22 +0300

    Update Backtesting.py
    Update Backtesting

--- crypto/pages/Backtesting.py
+++ crypto/pages/Backtesting.py
@@ -1,51 +1,55 @@
-from backtesting import Backtest,Strategy
-from backtesting.lib import crossover,plot_heatmaps
-import pandas as pd
+from backtesting import Backtest, Strategy
 import streamlit as st
 import talib
+import pandas as pd
+
 selected_indicators = st.multiselect("Select Technical Indicators", ['RSI', 'SMA', 'EMA', 'ADX'])
+
 class MyStrategy(Strategy):
-  upper_bound = st.sidebar.slider("Enter the Indicator Upper Limit", 0, 100)
-  lower_bound = st.sidebar.slider("Enter the Indicator Lower Limit", 0, 100)
-  time_period = st.sidebar.slider("Enter the Indicator Time Period", 0, 30)
-  def init(self):
-    self.indicators = {}
-    if 'RSI' in selected_indicators:
-      self.indicators['RSI'] = self.I(talib.RSI, self.data.Close, time_period)
-    if 'SMA' in selected_indicators:
-      self.indicators['SMA'] = self.I(talib.SMA, self.data.Close, timeperiod=time_period)
-    if 'EMA' in selected_indicators:
-      self.indicators['EMA'] = self.I(talib.EMA, self.data.Close, timeperiod=time_period)
-    if 'ADX' in selected_indicators:
-      self.indicators['ADX'] = self.I(talib.ADX, self.data.High, self.data.Low, self.data.Close, timeperiod=time_period)
+    def init(self):
+        self.indicators = {}
+        if 'RSI' in selected_indicators:
+            self.indicators['RSI'] = self.I(talib.RSI, self.data.Close, self.time_period)
+        if 'SMA' in selected_indicators:
+            self.indicators['SMA'] = self.I(talib.SMA, self.data.Close, timeperiod=self.time_period)
+        if 'EMA' in selected_indicators:
+            self.indicators['EMA'] = self.I(talib.EMA, self.data.Close, timeperiod=self.time_period)
+        if 'ADX' in selected_indicators:
+            self.indicators['ADX'] = self.I(talib.ADX, self.data.High, self.data.Low, self.data.Close, timeperiod=self.time_period)
 
     def next(self):
-      for indicator_name, indicator_values in self.indicators.items():
-        last_value = indicator_values[-1]
-        if indicator_name == 'RSI':
-          if last_value > upper_bound:
-            self.position.close()
-          elif last_value < lower_bound:
-            self.buy()
-        elif indicator_name == 'EMA' or indicator_name == 'ADX':
-          if last_value > upper_bound:
-            self.position.close()
-          elif last_value < lower_bound:
-               self.buy()
-#cash=st.sidebar.slider("Enter you available cash",0,10000,step=10)
+        for indicator_name, indicator_values in self.indicators.items():
+            last_value = indicator_values[-1]
+            if indicator_name == 'RSI':
+                if last_value > self.upper_bound:
+                    self.position.close()
+                elif last_value < self.lower_bound:
+                    self.buy()
+            elif indicator_name == 'EMA' or indicator_name == 'ADX':
+                if last_value > self.upper_bound:
+                    self.position.close()
+                elif last_value < self.lower_bound:
+                    self.buy()
+
+# Streamlit app
+st.title("Backtesting")
+
+# Sidebar inputs
+MyStrategy.upper_bound = st.sidebar.slider("Enter the Indicator Upper Limit", 0, 100)
+MyStrategy.lower_bound = st.sidebar.slider("Enter the Indicator Lower Limit", 0, 100)
+MyStrategy.time_period = st.sidebar.slider("Enter the Indicator Time Period", 0, 30)
+
+data = st.session_state['DataFrame']
+
+# Run backtest on button click
 if st.sidebar.button("Test my strategy"):
-  if st.session_state["DataFrame"] is not None:
-    bt=Backtest(st.session_state["DataFrame"],MyStrategy,cash=10000)
-    strategyStats=bt.run()
-    strategyStatsDF=pd.DataFrame(strategyStats)
-    #strategyTradesDF=pd.DataFrame(strategyStats['_trades'])
+    bt = Backtest(data, MyStrategy, cash=10000)
+    strategy_stats = bt.run()
+
     with st.container():
-      strategyStats_placeholder=st.empty()
-      strategyTrades_placeholder=st.empty()
-      with strategyStats_placeholder.expander("View Stategy Statistics"):
-        st.dataframe(strategyStatsDF)
-      with strategyTrades_placeholder.expander("View Strategy Trades"):
-        #st.dataframe(strategyTradesDF)
-        st.write(strategyStats['_equity_curve'])
-      
-      
+        # Display strategy statistics and equity curve
+        st.write("Strategy Statistics:")
+        st.dataframe(pd.DataFrame(strategy_stats['_strategy']))
+        
+        st.write("Equity Curve:")
+        st.line_chart(strategy_stats['_equity_curve'])
