98f4ce6c3052f30929f3acceefc624e9f995788f
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-08-18 06:26:10 +0300

    Update Backtesting.py
    Include streamlit multi-select for the various items.

--- crypto/pages/Backtesting.py
+++ crypto/pages/Backtesting.py
@@ -3,17 +3,32 @@ from backtesting.lib import crossover,plot_heatmaps
 import pandas as pd
 import streamlit as st
 import talib
+selected_indicators = st.multiselect("Select Technical Indicators", ['RSI', 'SMA', 'EMA', 'ADX']
 class MyStrategy(Strategy):
-  upper_bound=st.sidebar.slider("Enter the Indicator Upper Limit",0, 100)
-  lower_bound=st.sidebar.slider("Enter the Indicator Lower Limit",0, 100)
-  time_period=st.sidebar.slider("Enter the Indicator Time Period",0,30)
   def init(self):
-    self.indicator1=self.I(talib.RSI,self.data.Close,self.time_period)
-  def next(self):
-    if self.indicator1[-1]>self.upper_bound:
-      self.position.close()
-    elif self.indicator1[-1]<self.lower_bound:
-      self.buy()
+    self.indicators = {}
+    if 'RSI' in selected_indicators:
+      self.indicators['RSI'] = self.I(talib.RSI, self.data.Close, time_period)
+    if 'SMA' in selected_indicators:
+      self.indicators['SMA'] = self.I(talib.SMA, self.data.Close, timeperiod=time_period)
+    if 'EMA' in selected_indicators:
+      self.indicators['EMA'] = self.I(talib.EMA, self.data.Close, timeperiod=time_period)
+    if 'ADX' in selected_indicators:
+      self.indicators['ADX'] = self.I(talib.ADX, self.data.High, self.data.Low, self.data.Close, timeperiod=time_period)
+
+    def next(self):
+      for indicator_name, indicator_values in self.indicators.items():
+        last_value = indicator_values[-1]
+        if indicator_name == 'RSI':
+          if last_value > upper_bound:
+            self.position.close()
+          elif last_value < lower_bound:
+            self.buy()
+        elif indicator_name == 'EMA' or indicator_name == 'ADX':
+          if last_value > upper_bound:
+            self.position.close()
+          elif last_value < lower_bound:
+               self.buy()
 #cash=st.sidebar.slider("Enter you available cash",0,10000,step=10)
 if st.sidebar.button("Test my strategy"):
   if st.session_state["DataFrame"] is not None:
