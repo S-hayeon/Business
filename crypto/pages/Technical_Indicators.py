75e37460b8bb628ef31af5cd2b855a8a47dd6d04
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-08-19 14:10:48 +0300

    Update Technical_Indicators.py
    Line 15-17

--- crypto/pages/Technical_Indicators.py
+++ crypto/pages/Technical_Indicators.py
@@ -8,7 +8,10 @@ except:
     sys.path.append('/mount/src/business')
     from crypto import main
     from Technical_Analysis import Technical_indicators
-technicalIndicators=Technical_indicators.TIndicators(data=st.session_state['DataFrame'])
-#macd=technicalIndicators.MACD()
-bull_bear=technicalIndicators.BullBearish_state()
-st.write("The market trend is: ",bull_bear)
+# technicalIndicators=Technical_indicators.TIndicators(data=st.session_state['DataFrame'])
+# #macd=technicalIndicators.MACD()
+# bull_bear=technicalIndicators.BullBearish_state()
+# st.write("The market trend is: ",bull_bear)
+techanalysis=Technical_indicators.TIndicators(data=st.session_state['DataFrame'])
+indicatorsDF=techanalysis.techIndicators()
+st.dataframe(indicatorsDF)
