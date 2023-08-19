f8d5128f61864ab6ced102c648e08d01e30148c6
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-08-19 14:03:00 +0300

    Update Technical_Indicators.py
    Import module using the sys module.

--- fx/pages/Technical_Indicators.py
+++ fx/pages/Technical_Indicators.py
@@ -1,6 +1,12 @@
 import streamlit as st
-import talib
+import sys
+try:
+    sys.path.append('/app/business')
+    from Technical_Analysis import Technical_indicators
+except:  
+    sys.path.append('/mount/src/business')
+    from Technical_Analysis import Technical_indicators
 if __name__=='__main__':
-  techanalysis=TIndicators(st.session_state["DataFrame"])
+  techanalysis=Technical_indicators.TIndicators(st.session_state["DataFrame"])
   indicatorsDF=techanalysis.techIndicators()
   st.dataframe(indicatorsDF)
