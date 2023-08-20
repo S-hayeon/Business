e26d67222554839a8e635f608103bec2d21eaed1
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-08-20 20:58:21 +0300

    Update Testing.py

--- crypto/pages/Testing.py
+++ crypto/pages/Testing.py
@@ -3,37 +3,49 @@ from backtesting.test import GOOG
 import streamlit as st
 import talib
 import time
-import backtesting  # Import your backtesting library
+import backtesting
 
-def optimize_strategy():
-    class RSIOscillator(Strategy):
-      upper_bound = 70
-      lower_bound = 30
-      rsi_timeperiod=14
-      def init(self):
-        self.rsi = self.I(talib.RSI, self.data.Close,self.rsi_timeperiod)
-      def next(self):
+class RSIOscillator(Strategy):
+    def __init__(self, upper_bound, lower_bound, rsi_timeperiod):
+        self.upper_bound = upper_bound
+        self.lower_bound = lower_bound
+        self.rsi_timeperiod = rsi_timeperiod
+    
+    def init(self):
+        self.rsi = self.I(talib.RSI, self.data.Close, self.rsi_timeperiod)
+    
+    def next(self):
         if self.rsi[-1] > self.upper_bound:
-          self.position.close()
+            self.position.close()
         elif self.rsi[-1] < self.lower_bound:
-          self.buy()
-    bt=Backtest(GOOG,RSIOscillator,cash=10_000)
-    strategyStats=bt.optimize(
-    upper_bound=range(50,85,5), # Optimize Upper bound from 50-85 in steps of 5
-    lower_bound=range(10,50,5), # Optimize Lower bound from 10-50 in steps of 5
-    rsi_timeperiod=range(10,30,1), # Optimize the strategy with RSI timeperiod betweeen 10 and 30 in steps of 2
-)
+            self.buy()
+
+def optimize_strategy(upper_bound, lower_bound, rsi_timeperiod):
+    bt = Backtest(GOOG, RSIOscillator, cash=10_000, upper_bound=upper_bound, lower_bound=lower_bound, rsi_timeperiod=rsi_timeperiod)
+    strategy_stats = bt.run()
+    return strategy_stats
 
 def main():
     st.title("Backtesting Optimization Progress")
 
+    upper_bounds = range(50, 85, 5)
+    lower_bounds = range(10, 50, 5)
+    rsi_timeperiods = range(10, 30, 1)
+    
+    total_iterations = len(upper_bounds) * len(lower_bounds) * len(rsi_timeperiods)
+    progress = 0
+
     # Display a progress bar
     progress_bar = st.progress(0)
 
-    # Run the optimization and update progress bar
-    for progress in optimize_strategy():
-        progress_bar.progress(progress)
-    
+    # Iterate over optimization parameters
+    for upper_bound in upper_bounds:
+        for lower_bound in lower_bounds:
+            for rsi_timeperiod in rsi_timeperiods:
+                strategy_stats = optimize_strategy(upper_bound, lower_bound, rsi_timeperiod)
+                progress += 1
+                progress_bar.progress(progress / total_iterations)
+
     st.success("Optimization Complete!")
 
 if __name__ == "__main__":
