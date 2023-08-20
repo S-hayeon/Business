c2fc5fb8824fca2e2eb3ed7a539aced929013e12
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-08-20 21:00:50 +0300

    Update Testing.py

--- crypto/pages/Testing.py
+++ crypto/pages/Testing.py
@@ -6,22 +6,24 @@ import time
 import backtesting
 
 class RSIOscillator(Strategy):
-    def __init__(self, upper_bound, lower_bound, rsi_timeperiod):
-        self.upper_bound = upper_bound
-        self.lower_bound = lower_bound
-        self.rsi_timeperiod = rsi_timeperiod
-    
     def init(self):
-        self.rsi = self.I(talib.RSI, self.data.Close, self.rsi_timeperiod)
+        self.rsi = self.I(talib.RSI, self.data.Close, self.params.rsi_timeperiod)
     
     def next(self):
-        if self.rsi[-1] > self.upper_bound:
+        if self.rsi[-1] > self.params.upper_bound:
             self.position.close()
-        elif self.rsi[-1] < self.lower_bound:
+        elif self.rsi[-1] < self.params.lower_bound:
             self.buy()
 
 def optimize_strategy(upper_bound, lower_bound, rsi_timeperiod):
-    bt = Backtest(GOOG, RSIOscillator, cash=10_000, upper_bound=upper_bound, lower_bound=lower_bound, rsi_timeperiod=rsi_timeperiod)
+    bt = Backtest(
+        GOOG,
+        RSIOscillator,
+        cash=10_000,
+        upper_bound=upper_bound,
+        lower_bound=lower_bound,
+        rsi_timeperiod=rsi_timeperiod
+    )
     strategy_stats = bt.run()
     return strategy_stats
 
