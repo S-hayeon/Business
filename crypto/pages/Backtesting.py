8c7f13f562ddc4df03b03025890ecc4b55142008
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-08-21 11:39:17 +0300

    Update Backtesting.py
    Manual Backtesting mode.

--- crypto/pages/Backtesting.py
+++ crypto/pages/Backtesting.py
@@ -45,119 +45,60 @@ class MyStrategy(Strategy):
             self.position.close()
         elif self.should_buy:
             self.buy()
-
-
 ###################################################### Sidebar inputs ##############################
-mode=st.sidebar.radio("Do you have the indicators' values?",['Yes','No'])
-if mode=='Yes':
-    # Separate the indicator-specific sidebar inputs into their own blocks
-    for indicator_name in selected_indicators:
-        if indicator_name == 'ADX':
-            range_value_choice=st.sidebar.selectbox("Do you want one ADX value or range?",['One Value','Range'])
-            if range_value_choice=='One Value':
-                MyStrategy.adx_upper_bound=None
-                MyStrategy.adx_lower_bound = st.sidebar.slider("ADX Value, Buy/Sell when ADX is or above:", 0, 40, step=1)
-                MyStrategy.adx_time_period = st.sidebar.number_input("ADX Indicator Time Period", min_value=1, step=1)
+# Separate the indicator-specific sidebar inputs into their own blocks
+for indicator_name in selected_indicators:
+    if indicator_name == 'ADX':
+        range_value_choice=st.sidebar.selectbox("Do you want one ADX value or range?",['One Value','Range'])
+        if range_value_choice=='One Value':
+            MyStrategy.adx_upper_bound=None
+            MyStrategy.adx_lower_bound = st.sidebar.slider("ADX Value, Buy/Sell when ADX is or above:", 0, 40, step=1)
+            MyStrategy.adx_time_period = st.sidebar.number_input("ADX Indicator Time Period", min_value=1, step=1)
+            st.toast("Knowledge Nugget: ADX shows a trending market",icon="💹")
+        elif range_value_choice=='Range':
+            MyStrategy.adx_upper_bound = st.sidebar.slider("ADX Value, Buy/Sell when ADX is or below:", 0, 40, step=1)
+            MyStrategy.adx_lower_bound = st.sidebar.slider("ADX Value, Buy/Sell when ADX is or above:", 0, 40, step=1)
+            MyStrategy.adx_time_period = st.sidebar.number_input("Enter the ADX Indicator Time Period", min_value=1, step=1)
+            if MyStrategy.adx_upper_bound>MyStrategy.adx_lower_bound:
                 st.toast("Knowledge Nugget: ADX shows a trending market",icon="💹")
-            elif range_value_choice=='Range':
-                MyStrategy.adx_upper_bound = st.sidebar.slider("ADX Value, Buy/Sell when ADX is or below:", 0, 40, step=1)
-                MyStrategy.adx_lower_bound = st.sidebar.slider("ADX Value, Buy/Sell when ADX is or above:", 0, 40, step=1)
-                MyStrategy.adx_time_period = st.sidebar.number_input("Enter the ADX Indicator Time Period", min_value=1, step=1)
-                if MyStrategy.adx_upper_bound>MyStrategy.adx_lower_bound:
-                    st.toast("Knowledge Nugget: ADX shows a trending market",icon="💹")
-                else:
-                    st.toast("The ADX Upper limit should be higher the ADX Lower limit",icon="⚠️")
-            time.sleep(2)
-        elif indicator_name == 'EMA':
-            MyStrategy.ema_upper_bound = None
-            MyStrategy.ema_lower_bound = None
-            MyStrategy.ema_time_period = st.sidebar.number_input("Enter the EMA Indicator Time Period", min_value=1, step=1)
-            st.toast("Strategy buys when the last close is above the EMA")
-            time.sleep(2)
-        elif indicator_name == 'RSI':
-            MyStrategy.rsi_upper_bound = st.sidebar.slider("Enter the RSI Upper Limit", 0, 100, step=1)
-            MyStrategy.rsi_lower_bound = st.sidebar.slider("Enter the RSI Lower Limit", 0, 100, step=1)
-            MyStrategy.rsi_time_period = st.sidebar.number_input("Enter the RSI Time Period", min_value=1, step=1)
-            time.sleep(2)
-        elif indicator_name == 'SMA':
-            MyStrategy.sma_upper_bound = None
-            MyStrategy.sma_lower_bound = None
-            MyStrategy.sma_time_period = st.sidebar.number_input("Enter the SMA Indicator Time Period", min_value=1, step=1)
-            st.toast("Strategy buys when the last close is above the SMA")
-            time.sleep(2)
-    
-    data = st.session_state['DataFrame']
-    
-    # Run backtest on button click
-    if st.sidebar.button("Test my strategy"):
-        bt = Backtest(data, MyStrategy, cash=10000)
-        strategy_stats = bt.run()
-        with st.container():
-            # Display strategy statistics and equity curve
-            stats_placeholder=st.empty()
-            equity_placeholder=st.empty()
-            equity_percent_placeholder=st.empty()
-            with stats_placeholder.expander("Strategy Results"):
-                #st.dataframe(pd.DataFrame(strategy_stats['_strategy']))
-                st.dataframe(pd.DataFrame(strategy_stats))
-            with equity_placeholder.expander("Equity curve"):
-                st.write("Equity Curve:")
-                st.line_chart(strategy_stats['_equity_curve']['Equity'])
-            with equity_percent_placeholder.expander("Equity Drawdown curve"):
-                st.write("Equity Percentage Curve:")
-                st.line_chart(strategy_stats['_equity_curve']['DrawdownPct'])
-if mode=='No':
-    class ParameterOptimizer:
-        def __init__(self, data, strategy_class, cash=10000):
-            self.data = data
-            self.strategy_class = strategy_class
-            self.cash = cash
-            self.strategy_stats=None
-            self.wait_placeholder = st.empty()
-        
-        def optimize_parameters(self, strategy_name, parameter_name, lower_limit, upper_limit):
-            param_lower = st.sidebar.number_input(f"Lower limit of {parameter_name}", min_value=lower_limit, step=1)
-            param_upper = st.sidebar.number_input(f"Upper limit of {parameter_name}", min_value=upper_limit, step=1)
-            
-            if param_upper > param_lower:
-                param_range = range(param_lower, param_upper, 1)
-                if st.sidebar.button("Find optimal Values"):
-                    #self.strategy_stats = self.run_optimization(strategy_name, parameter_name, param_range)
-                    bt = Backtest(st.session_state['DataFrame'], MyStrategy, cash=10000)
-                    self.strategy_stats = self.strategy_stats = bt.optimize(parameter_name=param_range)
-                    self.wait_placeholder.write("Hold on as the system searches for optimal value", icon="🕐⌛")
             else:
-                st.toast(f"{parameter_name} upper is lower than {parameter_name} lower", icon="🚩")
-            return self.strategy_stats
-        
-        def run_optimization(self, strategy_name, parameter_name, param_range):
-            #selected_param_value = st.session_state[parameter_name]
-            # Call the Backtest and optimization methods here based on the strategy and parameter
-            # Example: bt = Backtest(self.data, self.strategy_class, cash=self.cash)
-            # Example: strategy_stats = bt.optimize({parameter_name}: param_range)
-            #bt = Backtest(self.data, self.strategy_class, cash=self.cash)
-            #strategy_stats = bt.optimize({parameter_name}: param_range)
-            # Return the strategy statistics
-            #return strategy_stats
-            #return None  # Placeholder for actual strategy statistics
-            pass
-    
-    # Example usage
-    data = st.session_state['DataFrame']    
-    parameter_optimizer = ParameterOptimizer(data, MyStrategy, cash=10000)
-    strategy_options = ['EMA', 'ADX', 'SMA']  # Add more strategies as needed
-    selected_strategy = st.selectbox("Select an Indicator to optimize:", strategy_options)
-    
-    if selected_strategy == 'EMA':
-        stats=parameter_optimizer.optimize_parameters('EMA', 'ema_time_period', 5, 100)
-    elif selected_strategy == 'ADX':
-        stats=parameter_optimizer.optimize_parameters('ADX', 'adx_time_period', 5, 100)
-    elif selected_strategy == 'SMA':
-        stats=parameter_optimizer.optimize_parameters('SMA', 'sma_time_period', 5, 100)
-    else:
-        pass
-    if stats is not None:
-        st.write(stats)
-        st.write(stats['_strategy'])
-    else:
-        st.sidebar.info("Find optimal values in strategy first")
+                st.toast("The ADX Upper limit should be higher the ADX Lower limit",icon="⚠️")
+        time.sleep(2)
+    elif indicator_name == 'EMA':
+        MyStrategy.ema_upper_bound = None
+        MyStrategy.ema_lower_bound = None
+        MyStrategy.ema_time_period = st.sidebar.number_input("Enter the EMA Indicator Time Period", min_value=1, step=1)
+        st.toast("Strategy buys when the last close is above the EMA")
+        time.sleep(2)
+    elif indicator_name == 'RSI':
+        MyStrategy.rsi_upper_bound = st.sidebar.slider("Enter the RSI Upper Limit", 0, 100, step=1)
+        MyStrategy.rsi_lower_bound = st.sidebar.slider("Enter the RSI Lower Limit", 0, 100, step=1)
+        MyStrategy.rsi_time_period = st.sidebar.number_input("Enter the RSI Time Period", min_value=1, step=1)
+        time.sleep(2)
+    elif indicator_name == 'SMA':
+        MyStrategy.sma_upper_bound = None
+        MyStrategy.sma_lower_bound = None
+        MyStrategy.sma_time_period = st.sidebar.number_input("Enter the SMA Indicator Time Period", min_value=1, step=1)
+        st.toast("Strategy buys when the last close is above the SMA")
+        time.sleep(2)
+
+data = st.session_state['DataFrame']
+
+# Run backtest on button click
+if st.sidebar.button("Test my strategy"):
+    bt = Backtest(data, MyStrategy, cash=10000)
+    strategy_stats = bt.run()
+    with st.container():
+        # Display strategy statistics and equity curve
+        stats_placeholder=st.empty()
+        equity_placeholder=st.empty()
+        equity_percent_placeholder=st.empty()
+        with stats_placeholder.expander("Strategy Results"):
+            #st.dataframe(pd.DataFrame(strategy_stats['_strategy']))
+            st.dataframe(pd.DataFrame(strategy_stats))
+        with equity_placeholder.expander("Equity curve"):
+            st.write("Equity Curve:")
+            st.line_chart(strategy_stats['_equity_curve']['Equity'])
+        with equity_percent_placeholder.expander("Equity Drawdown curve"):
+            st.write("Equity Percentage Curve:")
+            st.line_chart(strategy_stats['_equity_curve']['DrawdownPct'])
