6c61ee48367e97a09741b1df3015b8e24f681db1
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-08-22 08:58:56 +0300

    Update Testing.py
    Line 7 Filter the head and shoulder pattern

--- crypto/pages/Testing.py
+++ crypto/pages/Testing.py
@@ -4,7 +4,7 @@ from tradingpatterns.tradingpatterns import detect_head_shoulder
 def main():
     st.title("Head shoulders")
     head_shoulder=detect_head_shoulder(df=st.session_state["DataFrame"])
-    #head_shoulder[['Date','head_shoulder_pattern']]
+    head_shoulder[['head_shoulder_pattern']]
     st.write(st.session_state["DataFrame"].index)
     st.write(st.session_state["DataFrame"].columns)
     st.write(head_shoulder)
