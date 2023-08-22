be31be16155e8111e111d542dc3a1421642b35b7
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-08-22 09:16:01 +0300

    Update Testing.py
    Line 6-10 Multiple chart patterns

--- crypto/pages/Testing.py
+++ crypto/pages/Testing.py
@@ -1,13 +1,17 @@
 import streamlit as st
-from tradingpatterns.tradingpatterns import detect_head_shoulder
+from tradingpatterns import tradingpatterns
 
 def main():
     st.title("Head shoulders")
-    head_shoulder=detect_head_shoulder(df=st.session_state["DataFrame"])
-    head_shoulder[['head_shoulder_pattern']]
-    st.write(st.session_state["DataFrame"].index)
-    st.write(st.session_state["DataFrame"].columns)
-    st.write(head_shoulder)
+    head_shoulder=tradingpatterns.detect_head_shoulder(df=st.session_state["DataFrame"])
+    multiple_top_bottom=tradingpatterns.detect_multiple_tops_bottoms(df=st.session_state["DataFrame"])
+    triangle_pattern=tradingpatterns.detect_triangle_pattern(df=st.session_state["DataFrame"])
+    wedge_pattern=tradingpatterns.detect_wedge(df=st.session_state["DataFrame"])
+    double_topbottom_pattern=tradingpatterns.detect_double_top_bottom(df=st.session_state["DataFrame"])
+    #head_shoulder[['head_shoulder_pattern']]
+    #st.write(st.session_state["DataFrame"].index)
+    #st.write(st.session_state["DataFrame"].columns)
+    st.write(st.session_state["DataFrame"])
     
 
 if __name__ == "__main__":
