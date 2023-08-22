833d21f8e236ae0dfb79541a8d14383e4a282e27
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-08-22 08:55:20 +0300

    Update Testing.py
    Obtain Index

--- crypto/pages/Testing.py
+++ crypto/pages/Testing.py
@@ -1,15 +1,11 @@
-from backtesting import Backtest, Strategy
-from backtesting.test import GOOG
 import streamlit as st
-import talib
-import time
-import backtesting
 from tradingpatterns.tradingpatterns import detect_head_shoulder
 
 def main():
     st.title("Head shoulders")
     head_shoulder=detect_head_shoulder(df=st.session_state["DataFrame"])
-    head_shoulder[['Date','head_shoulder_pattern']]
+    #head_shoulder[['Date','head_shoulder_pattern']]
+    st.write(st.session_state["DataFrame"].index())
     st.write(head_shoulder)
     
 
