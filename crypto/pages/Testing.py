18a41a623868874f68c4da22cb8b08cc1d605776
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-08-22 09:20:39 +0300

    Update Testing.py
    Filter the data table

--- crypto/pages/Testing.py
+++ crypto/pages/Testing.py
@@ -3,15 +3,16 @@ from tradingpatterns import tradingpatterns
 
 def main():
     st.title("Head shoulders")
-    head_shoulder=tradingpatterns.detect_head_shoulder(df=st.session_state["DataFrame"])
-    multiple_top_bottom=tradingpatterns.detect_multiple_tops_bottoms(df=st.session_state["DataFrame"])
-    triangle_pattern=tradingpatterns.detect_triangle_pattern(df=st.session_state["DataFrame"])
-    wedge_pattern=tradingpatterns.detect_wedge(df=st.session_state["DataFrame"])
-    double_topbottom_pattern=tradingpatterns.detect_double_top_bottom(df=st.session_state["DataFrame"])
-    #head_shoulder[['head_shoulder_pattern']]
-    #st.write(st.session_state["DataFrame"].index)
-    #st.write(st.session_state["DataFrame"].columns)
-    st.write(st.session_state["DataFrame"])
+    data=st.session_state["DataFrame"]
+    head_shoulder=tradingpatterns.detect_head_shoulder(df=data)
+    multiple_top_bottom=tradingpatterns.detect_multiple_tops_bottoms(df=data)
+    triangle_pattern=tradingpatterns.detect_triangle_pattern(df=data)
+    wedge_pattern=tradingpatterns.detect_wedge(df=data)
+    double_topbottom_pattern=tradingpatterns.detect_double_top_bottom(df=data)
+    data[['head_shoulder_pattern','multiple_top_bottom_pattern','triangle_pattern','wedge_pattern','double_pattern']]
+    #st.write(data.index)
+    #st.write(data.columns)
+    st.write(data)
     
 
 if __name__ == "__main__":
