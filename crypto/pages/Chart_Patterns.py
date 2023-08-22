38c56a46c275142809be378ec8146e0618018550
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-08-22 10:40:42 +0300

    Update Chart_Patterns.py
    Line 38

--- crypto/pages/Chart_Patterns.py
+++ crypto/pages/Chart_Patterns.py
@@ -46,37 +46,37 @@ if st.session_state['CurrencyPair'] is not None and st.session_state['DataFrame'
         st.header(":green[Support] and :red[Resistance] Levels")
         candlestickfigure_placeholder = st.empty() # Create a placeholder for the candlestickfigure
         support_Resistance()
-    with st.expander("More info on Support and Resistance"):
-        #st.info("Sensitivity is the % of data the system looks back to find support and resistance.")
-        # Access support_resistance_lines from st.session_state
-        support_level = st.session_state.support_resistance_lines[0]
-        resistance_level = st.session_state.support_resistance_lines[1]
-        #st.markdown(f"**Support Level** is: {support_level}", unsafe_allow_html=True)
-        st.markdown(f":green[Support Level] is: {support_level}", unsafe_allow_html=True)
-        st.markdown(f":red[Resistance Level] is: {resistance_level}", unsafe_allow_html=True)
-        #st.write(f"The last candlestick is {candlestickID.candlestick_Pattern(len(st.session_state['DataFrame'])-1)[0]}")
-        #st.write(f"The last candlestick trend pattern is {candlestickID.candlestick_Pattern(len(st.session_state['DataFrame'])-1)[1]}")
-        # st.session_state['DataFrame']
-    candlestickID=chart_patterns.Pattern(data=st.session_state['DataFrame'])
-    candlestickDF=pd.DataFrame(columns=['Date', 'Pattern','Trend'])
-    for dfindex in range(len(st.session_state['DataFrame'])):
-        patternsID=candlestickID.candlestick_Pattern(dfindex)
-        #candlestickDF.at[dfindex,'Date']=st.session_state['DataFrame'].iloc[dfindex]['Date']
-        candlestickDF.at[dfindex,'Date']=st.session_state['DataFrame'].index[dfindex]
-        #candlestickDF.at[dfindex,'Pattern']=patternsID.max_pattern
-        pattern_name=str(patternsID[0])
-        candlestickDF.at[dfindex,'Pattern']=candlestickID.candlestick_dict[pattern_name]
-        candlestickDF.at[dfindex,'Trend']=patternsID[2]
-    chart_patterns_placeholder = st.empty() # Create a placeholder for the Chart Patterns
-    with chart_patterns_placeholder.expander("View Chart Patterns"):
-        data=st.session_state["DataFrame"]
-        head_shoulder=tradingpatterns.detect_head_shoulder(df=data)
-        multiple_top_bottom=tradingpatterns.detect_multiple_tops_bottoms(df=data)
-        triangle_pattern=tradingpatterns.detect_triangle_pattern(df=data)
-        wedge_pattern=tradingpatterns.detect_wedge(df=data)
-        double_topbottom_pattern=tradingpatterns.detect_double_top_bottom(df=data)
-        data=data[['head_shoulder_pattern','multiple_top_bottom_pattern','triangle_pattern','wedge_pattern','double_pattern']]
-    candlestickpatterns_placeholder = st.empty() # Create a placeholder for the candlestick Patterns Dataframe 
-    with candlestickpatterns_placeholder.expander("View Candlestick Patterns"):
-        st.dataframe(candlestickDF.dropna().reset_index().drop(columns=['index']))
+        chart_patterns_placeholder = st.empty() # Create a placeholder for the Chart Patterns
+        with st.expander("More info on Support and Resistance"):
+            #st.info("Sensitivity is the % of data the system looks back to find support and resistance.")
+            # Access support_resistance_lines from st.session_state
+            support_level = st.session_state.support_resistance_lines[0]
+            resistance_level = st.session_state.support_resistance_lines[1]
+            #st.markdown(f"**Support Level** is: {support_level}", unsafe_allow_html=True)
+            st.markdown(f":green[Support Level] is: {support_level}", unsafe_allow_html=True)
+            st.markdown(f":red[Resistance Level] is: {resistance_level}", unsafe_allow_html=True)
+            #st.write(f"The last candlestick is {candlestickID.candlestick_Pattern(len(st.session_state['DataFrame'])-1)[0]}")
+            #st.write(f"The last candlestick trend pattern is {candlestickID.candlestick_Pattern(len(st.session_state['DataFrame'])-1)[1]}")
+            # st.session_state['DataFrame']
+        candlestickID=chart_patterns.Pattern(data=st.session_state['DataFrame'])
+        candlestickDF=pd.DataFrame(columns=['Date', 'Pattern','Trend'])
+        for dfindex in range(len(st.session_state['DataFrame'])):
+            patternsID=candlestickID.candlestick_Pattern(dfindex)
+            #candlestickDF.at[dfindex,'Date']=st.session_state['DataFrame'].iloc[dfindex]['Date']
+            candlestickDF.at[dfindex,'Date']=st.session_state['DataFrame'].index[dfindex]
+            #candlestickDF.at[dfindex,'Pattern']=patternsID.max_pattern
+            pattern_name=str(patternsID[0])
+            candlestickDF.at[dfindex,'Pattern']=candlestickID.candlestick_dict[pattern_name]
+            candlestickDF.at[dfindex,'Trend']=patternsID[2]
+        with chart_patterns_placeholder.expander("View Chart Patterns"):
+            data=st.session_state["DataFrame"]
+            head_shoulder=tradingpatterns.detect_head_shoulder(df=data)
+            multiple_top_bottom=tradingpatterns.detect_multiple_tops_bottoms(df=data)
+            triangle_pattern=tradingpatterns.detect_triangle_pattern(df=data)
+            wedge_pattern=tradingpatterns.detect_wedge(df=data)
+            double_topbottom_pattern=tradingpatterns.detect_double_top_bottom(df=data)
+            data=data[['head_shoulder_pattern','multiple_top_bottom_pattern','triangle_pattern','wedge_pattern','double_pattern']]
+        candlestickpatterns_placeholder = st.empty() # Create a placeholder for the candlestick Patterns Dataframe 
+        with candlestickpatterns_placeholder.expander("View Candlestick Patterns"):
+            st.dataframe(candlestickDF.dropna().reset_index().drop(columns=['index']))
     
