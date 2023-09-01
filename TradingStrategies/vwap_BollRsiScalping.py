2de65a06790a7f602e2f6f2ea3fc2f86aec83c01
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-09-01 08:26:59 +0300

    Update vwap_BollRsiScalping.py
    Line 119 VWAPBOLLRSI class

--- TradingStrategies/vwap_BollRsiScalping.py
+++ TradingStrategies/vwap_BollRsiScalping.py
@@ -115,7 +115,8 @@ class MyVWAP_Boll_RSI_Strategy(Strategy):
     def __init__(self, *args, **kwargs):
         #self.sl_coeff = sl_coeff
         #self.tp_ratio = tp_ratio
-        super(MyVWAP_Boll_RSI_Strategy, self).__init__(*args, **kwargs)
+        #super(MyVWAP_Boll_RSI_Strategy, self).__init__(*args, **kwargs)
+        super(VWAPBOLLRSI, self).__init__(*args, **kwargs)
         #self.sl_coeff = kwargs['sl_coeff']  # Access sl_coeff from kwargs
         #self.tp_ratio = kwargs['tp_ratio']  # Access tp_ratio from kwargs
         self.signal1 = self.I(SIGNAL)
