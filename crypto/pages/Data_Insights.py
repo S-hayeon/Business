861b2bba60a47c3a25e2befdd9b72af4c0809ae2
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-09-14 14:26:09 +0300

    Create Data_Insights.py

---
+++ crypto/pages/Data_Insights.py
@@ -0,0 +1,16 @@
+import streamlit as st
+data=st.session_state['DataFrame']
+with st.expander("Coin Pair Data"):
+  st.dataframe(data)
+with st.expander("Descriptive Stats"):
+  st.dataframe(data.describe())
+  st.write(f'Median: {data.median()}')
+  st.write(f'Std Deviation: {data.median()}')
+  st.write(f'IQR:{data.median()}')
+st.header("Box Plot")
+box_plot=data['Close'].plot.box()
+box_plot.set_xlabel("index")
+box_plot.set_ylabel(f'{st.session_state['CoinPair']} Close values')
+st.pyplot(box_plot)
+        
+                 
