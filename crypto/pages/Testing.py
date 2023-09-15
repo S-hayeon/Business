00d405feefb4da4ee74db2c49e34084125f572d8
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-09-15 11:06:57 +0300

    Update Testing.py

--- crypto/pages/Testing.py
+++ crypto/pages/Testing.py
@@ -1,31 +1,33 @@
-from st_vizzu import *
 import streamlit as st
-df=st.session_state['DataFrame']
-obj=create_vizzu_obj(df)
-bar_obj=bar_chart(df,
-            x = "Date", 
-            y = "Close",
-            title= "Close Values Bar chart"
-            )
-anim_obj = beta_vizzu_animate( bar_obj,
-    x = "Date",
-    y =  ["Close", "Volume"],
-    title = "Close and Volume",
-    label= "Volume-Adjusted Close prices",
-    color="Genres",
-    legend="color",
-    sort="byValue",
-    reverse=True,
-    align="center",
-    split=False,
-)
-_dict = {"size": {"set": "Close"}, 
-    "geometry": "circle",
-    "coordSystem": "polar",
-    "title": "Animation for the OHLCV",
-    }
-anim_obj2 = vizzu_animate(anim_obj,_dict)
-vizzu_plot(anim_obj2)
+from streamlit.components.v1 import html
+import pandas as pd
+from ipyvizzu import Chart, Data, Config, Style
 
-        
-    
+st.title('🎈 ipyvizzu')
+
+def create_chart():
+
+    # initialize chart
+    chart = Chart(width="640px", height="360px", display="manual")
+
+    # add data
+    data = Data()
+    #data_frame = pd.read_csv("https://github.com/vizzuhq/ipyvizzu/raw/main/docs/examples/stories/titanic/titanic.csv")
+    data_frame = st.session_state['DataFrame']
+    data.add_data_frame(data_frame)
+
+    chart.animate(data)
+
+    # add config
+    chart.animate(Config({"x": "Date", "y": "Open", "label": "Date","title":"Open Prices"}))
+    chart.animate(Config({"x": ["Date","Close"], "label": ["Date","Close"], "color": "Close"}))
+    chart.animate(Config({"x": "Date", "y": ["High","Low"]}))
+
+    # add style
+    chart.animate(Style({"OHLCV data simulation": {"fontSize": 35}}))
+
+    return chart._repr_html_()
+
+
+CHART = create_chart()
+html(CHART, width=650, height=370)
