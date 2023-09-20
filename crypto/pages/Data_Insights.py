f18c181cb30545a5a48d24d773043df44241b761
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-09-20 11:27:22 +0300

    Update Data_Insights.py
    Data Insights Box Plot export and sending.

--- crypto/pages/Data_Insights.py
+++ crypto/pages/Data_Insights.py
@@ -33,22 +33,23 @@ box_plot = data[data_option].plot.box()
 box_plot.set_ylabel(f"{st.session_state['CoinPair']} {data_option} values")
 box_image_file_path = f"{st.session_state['CoinPair']}_boxPlot.png"
 plt.savefig(box_image_file_path)
-#st.pyplot(box_plot.figure)
-st.image(box_image_file_path)
+st.pyplot(box_plot.figure)
+#st.image(box_image_file_path) # Testing the exported image.
 def send_telegram_Message():
   bot_token=st.secrets['bot_token']
   chat_id=st.secrets['chat_id']
   url = f'https://api.telegram.org/bot{bot_token}/sendPhoto' # URL to the Telegram Bot API for sending photos
   #caption = f"Coin Pair:{st.session_state['CurrencyPair']}\nStart Date: {st.session_state['Start_Date']} to {st.session_state['End_Date']}\nInterval={st.session_state['Interval']}\nSupport Level: {st.session_state['support']}\nResistance Level: {st.session_state['resistance']}\n{st.session_state['DataFrame'].iloc[-1]}"
-  caption = f"Coin Pair:{st.session_state['CurrencyPair']}\nCrypto Token Category:{st.session_state['TokenCategory']}\nStart Date: {st.session_state['Start_Date']} to {st.session_state['End_Date']}\nInterval={st.session_state['Interval']}\n#CryptoGuideBotTrading"
+  caption = f"Coin Pair:{st.session_state['CurrencyPair']}\nCrypto Token Category:{st.session_state['TokenCategory']}\nStart Date: {st.session_state['Start_Date']} to {st.session_state['End_Date']}\nInterval={st.session_state['Interval']}\nBox Plot for the {data_option} values\n#CryptoGuideBotTrading"
   payload = {'chat_id': chat_id,'caption': caption}     
-  files = {'photo': open(image_file_path, 'rb')} # Prepare the payload
+  files = {'photo': open(box_image_file_path, 'rb')} # Prepare the payload
   response = requests.post(url, data=payload, files=files) # Send the photo
   if response.status_code == 200:
-    st.toast('Chart Patterns available!')
+    st.toast('Data Insights available!')
     if os.path.exists(image_file_path):
       os.remove(image_file_path)
   else:
       #st.toast('Failed to send photo. Status code:', response.status_code)
       st.toast(response.text)
+send_telegram_Message()
                  
