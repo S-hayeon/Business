9242934a96169581d59696367fe5a2dd39a84e01
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-09-23 15:00:29 +0300

    Update Data_Insights.py

--- crypto/pages/Data_Insights.py
+++ crypto/pages/Data_Insights.py
@@ -22,7 +22,7 @@ with st.expander("Descriptive Stats"):
   st.header("Measures of Central Tendency")
   stats_DF=pd.DataFrame({'Median': data[data_option].median(),
                      'Standard Deviation': data[data_option].std(),
-                     'InterQuartile Range':data[data_option].quantile(0.75)-data[data_option].quantile(0.25)})},index=['-'])
+                     'InterQuartile Range':data[data_option].quantile(0.75)-data[data_option].quantile(0.25)},index=['-'])
   st.dataframe(stats_DF)
   st.header("Percentiles")       
   percentileDF=pd.DataFrame({'Percentiles':data[data_option].quantile([0.05,0.25,0.5,0.75,0.95])}index=['-'])
