969fb09ca8466d898f7a896c6bf81f92266a6063
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-09-29 08:15:25 +0300

    Update News_Feed.py
    Header inclusion. And two wordclouds - news and multimedia.

--- crypto/pages/News_Feed.py
+++ crypto/pages/News_Feed.py
@@ -5,6 +5,7 @@ import requests
 import streamlit as st
 from wordcloud import WordCloud
 API_KEY=st.secrets['CP_API']
+st.title(f"{st.session_state['CoinPair'} NEWS FEED")
 def make_url(filter=None, currencies=None, kind=None, region=None, page=None):
     """Handle of URL variables for API POST."""
     url = 'https://cryptopanic.com/api/v1/posts/?auth_token={}'.format(API_KEY)
@@ -31,7 +32,8 @@ def make_url(filter=None, currencies=None, kind=None, region=None, page=None):
         url += "&page={}".format(page)
 
     return url
-url=make_url(currencies=st.session_state['Token'],kind='news',region='en')
+newsurl=make_url(currencies=st.session_state['Token'],kind='news',region='en')
+multimediaurl=make_url(currencies=st.session_state['Token'],kind='news',region='en')
 def get_page_json(url=None):
     """
     Get First Page.
@@ -45,7 +47,8 @@ def get_page_json(url=None):
     page = requests.get(url)
     data = page.json()
     return data
-response=get_page_json(url)['results']
+multimedia_response=get_page_json(multimediaurl)['results']
+news_response=get_page_json(newsurl)['results']
 def get_df(data):
     """Return pandas DF."""
     df = pd.DataFrame(data)
@@ -55,17 +58,31 @@ def get_df(data):
         pass
 
     return df
-df=get_df(response)
+multimedia_df=get_df(multimedia_response)
+news_df=get_df(news_response)
 def stripContents(text):
     return text.strip()
-title_list=df['title'].apply(stripContents).tolist()
-title_paragraph=" ".join(map(str,title_list))
-wordcloud=WordCloud(background_color='orange',max_words=1000,width=800,height=400).generate(title_paragraph)
+multimedia_title_list=multimedia_df['title'].apply(stripContents).tolist()
+news_title_list=news_df['title'].apply(stripContents).tolist()
+multimedia_paragraph=" ".join(map(str,multimedia_title_list))
+news_paragraph=" ".join(map(str,news_title_list))
+wordcloud=WordCloud(background_color='orange',max_words=1000,width=800,height=400).generate(multimedia_paragraph)
 plt.figure(figsize=(10,5))
 plt.imshow(wordcloud,interpolation='bilinear')
 plt.axis('off')
-wordcloudImage=f"{st.session_state['CoinPair']}_wordcloud.png"
-plt.savefig(wordcloudImage)  # Save as PNG
-st.image(wordcloudImage)
-if os.path.exists(wordcloudImage):
-      os.remove(wordcloudImage)
+media_wordcloud_img=f"{st.session_state['CoinPair']}_media_wordcloud.png"
+plt.savefig(media_wordcloud_img)  # Save as PNG
+st.header("Latest MultiMedia (e.g.,Youtube) Sentiments")
+st.image(media_wordcloud_img)
+if os.path.exists(media_wordcloud_img):
+      os.remove(media_wordcloud_img)
+wordcloud=WordCloud(background_color='orange',max_words=1000,width=800,height=400).generate(news_paragraph)
+plt.figure(figsize=(10,5))
+plt.imshow(wordcloud,interpolation='bilinear')
+plt.axis('off')
+news_wordcloud_img=f"{st.session_state['CoinPair']}_news_wordcloud.png"
+plt.savefig(news_wordcloud_img)  # Save as PNG
+st.header("Latest News Sentiments")
+st.image(news_wordcloud_img)
+if os.path.exists(news_wordcloud_img):
+      os.remove(news_wordcloud_img)
