ee0417a6d5afc926d39748805a3b03391298f798
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-09-29 09:08:30 +0300

    Update News_Feed.py
    Plot figure

--- crypto/pages/News_Feed.py
+++ crypto/pages/News_Feed.py
@@ -68,7 +68,7 @@ multimedia_paragraph=" ".join(map(str,multimedia_title_list))
 news_paragraph=" ".join(map(str,news_title_list))
 wordcloud=WordCloud(background_color='white',max_words=3000).generate(multimedia_paragraph)
 #wordcloud=WordCloud(background_color='white',max_words=1000,width=800,height=400).generate(multimedia_paragraph)
-plt.figure(figsize=(10,5))
+#plt.figure(figsize=(10,5))
 plt.imshow(wordcloud,interpolation='bilinear')
 plt.axis('off')
 media_wordcloud_img=f"{st.session_state['CoinPair']}_media_wordcloud.png"
