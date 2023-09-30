632a78592f93f1dfc711e0421cb341685c23e47d
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-09-30 16:37:48 +0300

    Create Tokenomics.py
    Crypto Tokenomics

---
+++ crypto/pages/Tokenomics.py
@@ -0,0 +1,16 @@
+import requests
+import streamlit as st
+# Define the cryptocurrency you want to scrape data for
+st.title(f"{st.session_state['CoinPair']} :yellow[Tokenomics]")
+crypto_symbol = "bitcoin"
+# Define the CoinGecko API endpoint for the cryptocurrency
+url = f"https://api.coingecko.com/api/v3/coins/{crypto_symbol}"
+# Send a GET request to the API endpoint
+response = requests.get(url)
+crypto_data = response.json()
+btc_total_supply=crypto_data['market_data']['total_supply']
+btc_max_supply=crypto_data['market_data']['max_supply']
+btc_circulating_supply=crypto_data['market_data']['circulating_supply']
+st.write(f"$BTC 24hr Total Supply:{btc_total_supply}")
+st.write(f"$BTC 24hr Max Supply:{btc_max_supply}")
+st.write(f"$BTC 24hr Circulating Supply:{btc_circulating_supply}")
