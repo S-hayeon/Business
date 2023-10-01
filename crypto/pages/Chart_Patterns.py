e9976d43b863a48aa6355fd1493c5abc695b3946
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-10-01 18:02:00 +0300

    Update Chart_Patterns.py
    Support and Resistance rounded off to 3 decimal places.

--- crypto/pages/Chart_Patterns.py
+++ crypto/pages/Chart_Patterns.py
@@ -40,8 +40,8 @@ if st.session_state['CurrencyPair'] is not None and st.session_state['DataFrame'
                      hlines=dict(hlines=support_resistance_lines,colors=['g','r'],linestyle='-.'),
                      savefig=image_file_path)
         #candlestickfigure_placeholder.pyplot(fig)
-        st.session_state['support']=support_resistance_lines[0]
-        st.session_state['resistance']=support_resistance_lines[1]
+        st.session_state['support']=round(float(support_resistance_lines[0]),3)
+        st.session_state['resistance']=round(float(support_resistance_lines[1]),3)
         #st.pyplot(fig)
         #plt.savefig(f"{image_file_path}",dpi=1400)
         st.image(image_file_path)
