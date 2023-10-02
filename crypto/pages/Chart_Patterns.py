cb30983ac94d97a19af45b25c1381a7cc24c722e
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-10-02 06:50:26 +0300

    Update Chart_Patterns.py
    Support

--- crypto/pages/Chart_Patterns.py
+++ crypto/pages/Chart_Patterns.py
@@ -40,7 +40,8 @@ if st.session_state['CurrencyPair'] is not None and st.session_state['DataFrame'
                      hlines=dict(hlines=support_resistance_lines,colors=['g','r'],linestyle='-.'),
                      savefig=image_file_path)
         #candlestickfigure_placeholder.pyplot(fig)
-        st.session_state['support']=lambda round(float(support_resistance_lines[0]), 2): round(float(support_resistance_lines[0]),2) if support_resistance_lines[0]>1 else round(support_resistance_lines[0],4)
+        #st.session_state['support']=lambda round(float(support_resistance_lines[0]), 2): round(float(support_resistance_lines[0]),2) if support_resistance_lines[0]>1 else round(support_resistance_lines[0],4)
+        st.session_state['support']=lambda lambda x=support_resistance_lines[0]: round(float(x), 2) if float(x) > 1 else round(float(x), 4)
         st.session_state['resistance']=lambda round(float(support_resistance_lines[1]), 2): round(float(support_resistance_lines[1]),2) if support_resistance_lines[1]>1 else round(support_resistance_lines[1],4)
         #st.pyplot(fig)
         #plt.savefig(f"{image_file_path}",dpi=1400)
