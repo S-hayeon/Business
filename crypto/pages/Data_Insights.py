57b609847fdbfe9369b15a35da8a0d44adf3fd13
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-10-02 09:33:24 +0300

    Update Data_Insights.py
    Date time ignored

--- crypto/pages/Data_Insights.py
+++ crypto/pages/Data_Insights.py
@@ -52,7 +52,7 @@ def send_twitter_Message():
   api = tweepy.API(auth)
   media = api.media_upload(filename=box_image_file_path)
   media_id = media.media_id
-  caption = f"Coin Pair:{st.session_state['CurrencyPair']}\nCrypto Token: ${st.session_state['Token']}\nCrypto Token Category:{st.session_state['TokenCategory']}\nInterval={st.session_state['Interval']}\nBox Plot for the {data_option} values as of{currentDatetime}\n{box_plot_caption}#CryptoTradingGuideBot."
+  caption = f"Coin Pair:{st.session_state['CurrencyPair']}\nCrypto Token: ${st.session_state['Token']}\nCrypto Token Category:{st.session_state['TokenCategory']}\nInterval={st.session_state['Interval']}\nBox Plot for the {data_option} values\n{box_plot_caption}#CryptoTradingGuideBot."
   client.create_tweet(media_ids=[media_id], text=caption)
   st.toast('Data Insights available!')
   if os.path.exists(box_image_file_path):
