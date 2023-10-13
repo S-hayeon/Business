290885718e6d426ef07247fc4619ca713bd17021
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-10-13 15:09:39 +0300

    Update Tokenomics.py
    Try Except block statement.

--- crypto/pages/Tokenomics.py
+++ crypto/pages/Tokenomics.py
@@ -11,18 +11,20 @@ if response.status_code==200:
   token_data = response.json()
 else:
   st.toast("Data Fetch Error")
+value_id=None
 try:
   for key,value in token_data['data'].items():
     if value.get('symbol')==crypto_symbol:
       value_id=value['id']
+      tokenomics_data=token_data['data'][str(value_id)]
+      token_market_cap=tokenomics_data['quotes']['USD']['market_cap']
+      token_total_supply=tokenomics_data['total_supply']
+      token_max_supply=tokenomics_data['max_supply']
+      token_circulating_supply=tokenomics_data['circulating_supply']
+      st.write(f"${crypto_symbol} Market Cap in $USD:{token_market_cap}")
+      st.write(f"${crypto_symbol} 24hr Total Supply:{token_total_supply}")
+      st.write(f"${crypto_symbol} 24hr Max Supply:{token_max_supply}")
+      st.write(f"${crypto_symbol} 24hr Circulating Supply:{token_circulating_supply}")
 except:
   st.toast("Coin Pair Tokenomics not available")
-tokenomics_data=token_data['data'][str(value_id)]
-token_market_cap=tokenomics_data['quotes']['USD']['market_cap']
-token_total_supply=tokenomics_data['total_supply']
-token_max_supply=tokenomics_data['max_supply']
-token_circulating_supply=tokenomics_data['circulating_supply']
-st.write(f"${crypto_symbol} Market Cap in $USD:{token_market_cap}")
-st.write(f"${crypto_symbol} 24hr Total Supply:{token_total_supply}")
-st.write(f"${crypto_symbol} 24hr Max Supply:{token_max_supply}")
-st.write(f"${crypto_symbol} 24hr Circulating Supply:{token_circulating_supply}")
+
