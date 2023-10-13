9cda71b62479156dde2d9c28c71d1138a7e4c5d5
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-10-13 14:31:24 +0300

    Update Tokenomics.py

--- crypto/pages/Tokenomics.py
+++ crypto/pages/Tokenomics.py
@@ -2,15 +2,23 @@ import requests
 import streamlit as st
 # Define the cryptocurrency you want to scrape data for
 st.title(f"{st.session_state['CurrencyPair']}:orange[ Tokenomics]")
-crypto_symbol = "bitcoin"
+crypto_symbol = st.session_state['Token']
 # Define the CoinGecko API endpoint for the cryptocurrency
-url = f"https://api.coingecko.com/api/v3/coins/{crypto_symbol}"
+url = f"https://api.alternative.me/v2/ticker/"
 # Send a GET request to the API endpoint
 response = requests.get(url)
-crypto_data = response.json()
-btc_total_supply=crypto_data['market_data']['total_supply']
-btc_max_supply=crypto_data['market_data']['max_supply']
-btc_circulating_supply=crypto_data['market_data']['circulating_supply']
-st.write(f"$BTC 24hr Total Supply:{btc_total_supply}")
-st.write(f"$BTC 24hr Max Supply:{btc_max_supply}")
-st.write(f"$BTC 24hr Circulating Supply:{btc_circulating_supply}")
+if response.status_code==200:
+  token_data = response.json()
+else:
+  st.toast("Data Fetch Error")
+for key,value in token_data['data'].items():
+  if value.get('symbol')==crypto_symbol:
+    value_id=value['id']
+tokenomics_data=token_data['data'][str(value_id)]
+token_market_cap=tokenomics_data['quotes']['market_cap']
+token_total_supply=tokenomics_data['total_supply']
+token_max_supply=tokenomics_data['max_supply']
+token_circulating_supply=tokenomics_data['circulating_supply']
+st.write(f"${crypto_symbol} 24hr Total Supply:{token_total_supply}")
+st.write(f"${crypto_symbol} 24hr Max Supply:{token_max_supply}")
+st.write(f"${crypto_symbol} 24hr Circulating Supply:{token_circulating_supply}")
