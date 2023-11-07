e9b928f086b7bfccabbb61e7929a06afa6cb8068
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-11-07 10:01:58 +0300

    Update AI Predictions.py
    Title and Header text. Predictions table

--- crypto/pages/AI Predictions.py
+++ crypto/pages/AI Predictions.py
@@ -27,27 +27,23 @@ MSE = mean_squared_error(close_data, closeData_KNN_predict)
 R2_Score = round((r2_score(close_data, closeData_KNN_predict))*100,3)
 
 # Create a Streamlit app
-st.title("BTC-USDT Predicted Data")
-st.write("Predicted prices using K-Nearest Neighbors Regressor")
-# st.write(f"Mean Squared Error: {MSE}")
-st.write(f"Accuracy Score% : {R2_Score}")
-
-# Display the plot
-plt.title("BTC-USDT Predicted data")
-plt.scatter(open_data, closeData_KNN_predict, label='Predicted data')
-st.pyplot(plt)
-open_data = data.Open.values.reshape(-1, 1)
-close_data = data.Close.values.reshape(-1, 1)
-closeData_KNN_predict = closeData_KNN_predict.reshape(-1, 1)
-# Create a DataFrame
-# predictions_table = pd.DataFrame({
-#     "Open": open_data,
-#     "Close": close_data,
-#     "Close (Predicted)": closeData_KNN_predict
-# })
-#predictions_table = pd.DataFrame({"Close (Predicted)": closeData_KNN_predict})
-# Optionally, display more details
-if st.sidebar.checkbox("Show Data Details"):
+st.title(f"Artificial Intelligence Price Predictions")
+with st.expander(f"{st.session_state['CoinPair']} AI Price Predictions"):
+    st.write("Predicted prices using K-Nearest Neighbors Regressor")
+    # st.write(f"Mean Squared Error: {MSE}")
+    st.write(f"Accuracy Score% : {R2_Score}")
+    # Display the plot
+    plt.title("BTC-USDT Predicted data")
+    plt.scatter(open_data, closeData_KNN_predict, label='Predicted data')
+    st.pyplot(plt)
+    open_data = data.Open.values.reshape(-1, 1)
+    close_data = data.Close.values.reshape(-1, 1)
+    closeData_KNN_predict = closeData_KNN_predict.reshape(-1, 1)
+    # Create a DataFrame
+    # predictions_table = pd.DataFrame({"Open": open_data,"Close": close_data,"Close (Predicted)": closeData_KNN_predict})
+    predictions_table = pd.DataFrame({"Close (Predicted)": closeData_KNN_predict})
+    # Optionally, display more details
+    if st.checkbox("Show Data Details"):
     st.write(f"{st.session_state['CoinPair']} Price Predictions Data Table")
     st.dataframe(closeData_KNN_predict)
 
