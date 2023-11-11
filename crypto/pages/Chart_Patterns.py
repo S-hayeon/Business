ff5f0b8046dc5638515b35a73ab0f20f5339c199
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-11-11 16:26:20 +0300

    Update Chart_Patterns.py
    Title formatting.

--- crypto/pages/Chart_Patterns.py
+++ crypto/pages/Chart_Patterns.py
@@ -35,7 +35,7 @@ if st.session_state['CurrencyPair'] is not None and st.session_state['DataFrame'
         support_resistance_lines=list(chart_pattern.support_resistance())
         #support_resistance_lines=list(chart_pattern.support_resistance(int(lookback)))
         st.session_state['support_resistance_lines'] = support_resistance_lines
-        fig=mpf.plot(st.session_state['DataFrame'],type='candle',title=f"{st.session_state['CoinPair']} Support Resistance Plot {st.session_state['StartDate']} to {st.session_state['EndDate']}"
+        fig=mpf.plot(st.session_state['DataFrame'],type='candle',title=f"{st.session_state['CoinPair']} Support Resistance Plot {st.session_state['StartDate']} to {st.session_state['EndDate']}",
                      volume=True,style='binance',
                      hlines=dict(hlines=support_resistance_lines,colors=['g','r'],linestyle='-.'),
                      savefig=image_file_path)
