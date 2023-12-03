6a9a2aed2cdfe87ad141afce76f74cb01bd03da4
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-12-03 20:56:24 +0300

    Update Tokenomics.py
    Todays Tip feature added

--- crypto/pages/Tokenomics.py
+++ crypto/pages/Tokenomics.py
@@ -1,5 +1,7 @@
+import re
 import requests
 import streamlit as st
+import tweepy
 # Define the cryptocurrency you want to scrape data for
 st.title(f"{st.session_state['CurrencyPair']}:orange[ Tokenomics]")
 crypto_symbol = st.session_state['Token']
@@ -12,6 +14,41 @@ if response.status_code==200:
 else:
   st.toast("Data Fetch Error")
 value_id=None
+link='https://asiliventures.com/index.php/2023/12/03/crypto-dictionary-glossary/'
+response = requests.get(link)
+html_content = response.content
+soup = BeautifulSoup(html_content, 'html.parser')
+all_list=soup.find_all("li")
+first_item=8
+count=86
+glossary=all_list[first_item:first_item+count]
+random_index=random.randint(0,count-1)
+tip=str(glossary[random_index])
+# Use a regular expression to remove HTML tags
+today_tip = re.sub(r'<.*?>', '',tip)
+today_tip_caption=f"Trading 📊Tip💡 {today_tip}"
+def send_telegram_Message():
+  bot_token=st.secrets['bot_token']
+  chat_id=st.secrets['chat_id']
+  url = f'https://api.telegram.org/bot{bot_token}/sendMessage' # URL to the Telegram Bot API for sending photos
+  #caption = f"Coin Pair:{st.session_state['CurrencyPair']}\nStart Date: {st.session_state['Start_Date']} to {st.session_state['End_Date']}\nInterval={st.session_state['Interval']}\nSupport Level: {st.session_state['support']}\nResistance Level: {st.session_state['resistance']}\n{st.session_state['DataFrame'].iloc[-1]}"
+  payload = {'chat_id': chat_id,'caption': today_tip_caption}     
+  response = requests.post(url, data=payload) # Send the Message
+  if response.status_code == 200:
+    st.toast('Tokenomics available!')
+    else:
+      #st.toast('Failed to send photo. Status code:', response.status_code)
+      st.toast(response.text)
+def send_twitter_Message():
+  apiKey= st.secrets['apiKey']
+  apiSecret=st.secrets['apiSecret']
+  bearerToken=st.secrets['bearerToken']
+  accessToken=st.secrets['accessToken']
+  accessSecret=st.secrets['accessSecret']
+  client = tweepy.Client(bearer_token=bearerToken,consumer_key=apiKey,consumer_secret=apiSecret,access_token=accessToken,access_token_secret=accessSecret)
+  auth = tweepy.OAuthHandler(apiKey, apiSecret)
+  auth.set_access_token(accessToken,accessSecret) 
+  client.create_tweet(text=today_tip_caption)
 try:
   for key,value in token_data['data'].items():
     if value.get('symbol')==crypto_symbol:
@@ -25,6 +62,8 @@ try:
       st.write(f"${crypto_symbol} 24hr Total Supply:{token_total_supply}")
       st.write(f"${crypto_symbol} 24hr Max Supply:{token_max_supply}")
       st.write(f"${crypto_symbol} 24hr Circulating Supply:{token_circulating_supply}")
+      send_twitter_Message()
+      send_telegram_Message()
 except:
   st.toast("Coin Pair Tokenomics not available")
 
