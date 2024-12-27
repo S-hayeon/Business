42f8084bcf759a1c341b0e69fb0f2b31fcedd118
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2024-12-27 08:58:47 +0300

    Update CryptoAnalysis.py
    fig,ax=plt.subplots()

--- crypto/CryptoAnalysis.py
+++ crypto/CryptoAnalysis.py
@@ -108,7 +108,8 @@ def visualize_data(df,title_text):
     if df is not None:
         #st.session_state['DataFrame']=df
         title_placeholder.header(f"{st.session_state['CurrencyPair']} Crypto Analysis")
-        fig=mpf.plot(df,type='line',volume=True,style='binance')
+        fig,ax=plt.subplots()
+        mpf.plot(df,type='line',volume=True,style='binance')
         candlestickfigure_placeholder.pyplot(fig)
         with df_expander_placeholder.expander(f"View the {title_text} data"):
             #data_placeholder.dataframe(df)
