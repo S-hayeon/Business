2ff7cd1cf029a33b348ba15011562132d7e6d229
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2025-01-10 10:31:22 +0300

    Update CryptoAnalysis.py
    Date set_index and convert this with unit='ms'

--- crypto/CryptoAnalysis.py
+++ crypto/CryptoAnalysis.py
@@ -114,6 +114,8 @@ def get_historical_data(symbol, interval, startTime, endTime):
     # Convert timestamp to datetime 
     df["Open Time"] = pd.to_datetime(df["Open Time"], unit='ms') 
     df["Close Time"] = pd.to_datetime(df["Close Time"], unit='ms')
+    df = df.set_index('Date')  # Note this is in ms
+    df.index = pd.to_datetime(df.index, unit='ms')
     return df
 def round_value(input_value):
     if input_value>1:
